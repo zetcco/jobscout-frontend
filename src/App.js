2d4be1274d48d2606e8e11d15b65f68816cf9b9c
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-01-18 12:44:11 +0530

    Added centered layout component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import { CenteredContent } from "./components/layout/CenteredContent";
 import { NavigationLayout } from "./components/layout/NavigationLayout";
 import { RootLayout } from "./components/layout/RootLayout";
 import { Home } from "./routes/Home";
 import { Login } from "./routes/Login";
-import { Signup } from "./routes/Signup";
+import { SelectAccountType } from "./routes/signup/SelectAccountType";
 
 const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={<RootLayout/>}>
+
       <Route path="/" element={<NavigationLayout/>}>
         <Route path="home" element={<Home/>}/>
       </Route>
-      <Route path="/">
+
+      <Route path="/" element={<CenteredContent/>}>
         <Route path="login" element={<Login/>}/>
-        <Route path="signup" element={<Signup/>}/>
+        <Route path="signup">
+          <Route path="type" element={ <SelectAccountType/> }/>
+        </Route>
       </Route>
+
     </Route>
 ))
 
