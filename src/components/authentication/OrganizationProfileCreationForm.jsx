d216c6df892488cab63752ef32a4cb86e0319445
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-01-18 15:22:09 +0530

    Dashed area

--- src/components/authentication/OrganizationProfileCreationForm.jsx
+++ src/components/authentication/OrganizationProfileCreationForm.jsx
@@ -1,17 +1,18 @@
-import { Box, Button, TextField, Typography } from '@mui/material'
+import { Box, Button, Stack, TextField, Typography } from '@mui/material'
 import React from 'react'
 import { CenteredHeaderCard } from '../cards/CenteredHeaderCard'
+import { DashedArea } from '../input/DashedArea'
 
 export const OrganizationProfileCreationForm = () => {
   return (
     <CenteredHeaderCard
         title={"Create your Profile"} 
         footer={<Button sx={{ width: '100%' }}>Continue</Button>}
-        icon={<Typography>X</Typography>}
     >
-        <TextField id="outlined-basic" label="Outlined" variant="outlined"/>
-        <Box width={'100%'} height={25} sx={{ backgroundColor: '#f00' }}>
-        </Box>
+        <Stack spacing={2} sx={{ px: 2, width: '100%' }}>
+            <TextField id="outlined-basic" label="Outlined" variant="outlined"/>
+            <DashedArea text={"Click here to Upload Logo"}/>
+        </Stack>
     </CenteredHeaderCard>
   )
 }
