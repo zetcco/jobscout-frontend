c12547316aec9bfd94b65ab2a19a617d8587d9fb
ANMSilva <nipunmadumal18@gmail.com>
2023-01-21 10:01:35 +0530

    SmallPanel Added

---
+++ src/components/SmallPanel.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Stack } from "@mui/system";
+import { Typography} from "@mui/material";
+import { BasicCard } from "./BasicCard";
+
+
+const SmallPanel = ({mainTitle ,children}) => {
+    return ( 
+        <BasicCard>
+            <Stack direction={'column'} spacing={2}>
+        `       <Stack>
+                        <Typography variant='h5'>{ mainTitle }</Typography>
+                </Stack>
+                        { children }
+            </Stack>
+        </BasicCard>
+     );
+}
+ 
+export default SmallPanel;
