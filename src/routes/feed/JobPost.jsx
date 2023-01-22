32e257f808c656b9612fad4a68a04698253645aa
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-01-22 13:23:25 +0530

    closes #7

--- src/routes/feed/JobPost.jsx
+++ src/routes/feed/JobPost.jsx
@@ -11,9 +11,7 @@ export const JobPost = () => {
     return ( 
         <BasicCard>
             <Stack spacing={2} sx={{ width: '100%' }}>
-                        <Box>
-                            <SingleJobPost/>
-                        </Box>
+                        <Box> <SingleJobPost/> </Box>
                         <Stack direction={{ xs: "column", sm: "row" }} alignItems="stretch" justifyContent={"space-between"} spacing={2}>
                             <SmallPanel
                                 mainTitle={'Ownership'}
