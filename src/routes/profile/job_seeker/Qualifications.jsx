8df247c4b20f9fd703aebba5e6c94f7b0d0ffd9f
ANMSilva <nipunmadumal18@gmail.com>
2023-01-25 02:29:25 +0530

    Job_seeker Qualifications created

---
+++ src/routes/profile/job_seeker/Qualifications.jsx
@@ -0,0 +1,31 @@
+import { Button } from '@mui/material'
+import React from 'react'
+import { Profile } from '../../../components/profile/Profile'
+import { ProfileWithFullNameSubtitle } from '../../../components/profile/ProfileWithFullNameSubtitle'
+import AddIcon from '@mui/icons-material/Add';
+import { EducationalCard } from '../../../components/profile/education/EducationalCard';
+
+export const Qualification = () => {
+  return (
+        <Profile
+            profileIcon={<ProfileWithFullNameSubtitle name = {'Nipun Madumal'} subtitle={'Creative Software'}/>}
+            buttonSet1 ={[
+                <Button variant = {'outlined'} startIcon = {<AddIcon/>}>Message</Button> , 
+                <Button variant = {'outlined'} startIcon = {<AddIcon/>}>Add Recommendations</Button>
+            ]}
+
+            buttonSet2 ={[
+                <Button variant = {'outlined'}>Recommendations</Button> , 
+                <Button variant = {'outlined'}>Posts</Button>,
+                <Button variant = {'outlined'}>Qualification</Button> , 
+                <Button variant = {'outlined'}>Portfolio</Button>
+            ]}
+
+            contend = {[
+                <EducationalCard/>,
+                <EducationalCard/>,
+                <EducationalCard/>,
+            ]}
+        />
+  )
+}
