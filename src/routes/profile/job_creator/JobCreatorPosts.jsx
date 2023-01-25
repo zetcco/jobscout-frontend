c6f949743cf3029be3ada998b99b92b3bfc2d457
ANMSilva <nipunmadumal18@gmail.com>
2023-01-25 10:09:32 +0530

    JobCreatorPosts created

---
+++ src/routes/profile/job_creator/JobCreatorPosts.jsx
@@ -0,0 +1,31 @@
+import { Button } from '@mui/material'
+import React from 'react'
+import { Profile } from '../../../components/profile/Profile'
+import { ProfileWithFullNameSubtitle } from '../../../components/profile/ProfileWithFullNameSubtitle'
+import AddIcon from '@mui/icons-material/Add';
+import { Comments } from '../../../components/SocialMedia/Comments';
+
+export const JobCreatorPosts = () => {
+  return (
+        <Profile
+            profileIcon={<ProfileWithFullNameSubtitle name = {'Nipun Madumal'} subtitle={'Job Seekeer'}/>}
+            buttonSet1 ={[
+                <Button variant = {'outlined'} startIcon = {<AddIcon/>}>Message</Button> , 
+                <Button variant = {'outlined'} startIcon = {<AddIcon/>}>Add Recommendations</Button>
+            ]}
+
+            buttonSet2 ={[
+                <Button variant = {'outlined'}>Recommendations</Button> , 
+                <Button variant = {'outlined'}>Posts</Button>,
+                <Button variant = {'outlined'}>Qualification</Button> , 
+                <Button variant = {'outlined'}>Portfolio</Button>
+            ]}
+
+            contend = {[
+                <Comments name = {'Nipun Madumal'}/> , 
+                <Comments name = {'Rajitha Thilanka'}/> ,
+                <Comments name = {'Praveen Chamod'}/> 
+            ]}
+        />
+  )
+}
