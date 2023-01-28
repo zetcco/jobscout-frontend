3c851a5227d9676d867973f06e7c2703de93dcf3
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-01-28 18:27:49 +0530

    Login form fix

--- src/components/authentication/SigninForm.jsx
+++ src/components/authentication/SigninForm.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import TextField from '@mui/material/TextField';
-import { Avatar, Button, Grid, Stack, Typography} from '@mui/material';
+import { Avatar, Button, Stack, Typography} from '@mui/material';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import { CenteredHeaderCard } from "../cards/CenteredHeaderCard";
 import { Link as RouterLink } from "react-router-dom";
@@ -13,17 +13,17 @@ const SigninForm = () => {
         footer = {<Button component={RouterLink} to={"/signup/type"} variant="contained" fullWidth>Register</Button>}
         children = {
             <Stack direction={'column'} spacing={15} sx={{ width: '100%' }}>
-                <Stack direction={'column'} spacing = {4}>
+                <Stack direction={'column'} spacing = {2}>
                     <Stack spacing={1} flexGrow = {1} direction = {'column'}>         
                         <TextField 
                             id="outlined-basic" 
                             label="Email" 
                             variant="outlined"
                             placeholder = "youremail@example.com"
+                            helperText="Enter your email"
                             fullWidth 
                             required
                             />
-                        <Typography>Enter your email</Typography>
                     </Stack>
 
                     <Stack spacing={1} flexGlow = {1} direction = {'column'}>
@@ -32,11 +32,11 @@ const SigninForm = () => {
                             label="Password" 
                             variant="outlined"
                             placeholder = "Your Password"
+                            helperText="Enter your password"
                             type = "password"
                             fullWidth 
                             required
                         />
-                        <Typography>Enter your password</Typography>
                     </Stack>
 
                     <Stack flexGrow={1}>
@@ -45,7 +45,7 @@ const SigninForm = () => {
                 </Stack>
                 <Stack>
                     <Stack align= {'center'} flexGrow={1}>
-                        <Typography variant = {'h6'}>Don't you have an Account?</Typography>
+                        <Typography variant='body'>Don't you have an Account?</Typography>
                     </Stack>
                 </Stack>
         </Stack> 
