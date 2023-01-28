e96bde2b5d413e97d0e22cc43314fa22a0d2146e
Gimhan Mahela <gimhanmahela@gmail.com>
2023-01-28 21:45:59 +0530

    Create AddCompany

---
+++ src/routes/profile/job_creator/AddCompany.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AddCompanyForm from '../../../components/authentication/user/job_creator/AddCompanyForm'
+
+const AddCompany = () => {
+  return (
+    <AddCompanyForm/>
+  )
+}
+
+export default AddCompany
