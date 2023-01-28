f59ef4c7ddb338aa74fc64f4b0a98a1141d99a87
ANMSilva <nipunmadumal18@gmail.com>
2023-01-28 12:25:47 +0530

    PrifileWithFullNameSubtitleSmall modifed  , Text.jsx modified

--- src/components/profile/ProfileWithFullNameSubtitleSmall.jsx
+++ src/components/profile/ProfileWithFullNameSubtitleSmall.jsx
@@ -2,14 +2,14 @@ import { Stack } from "@mui/system";
 import { FormHelperText ,Typography} from "@mui/material";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
-export const ProfileWithFullNameSubtitle = ({title , name , subtitle}) => {
+export const ProfileWithFullNameSubtitleSmall = ({title , name , subtitle}) => {
     return ( 
             <Stack direction={'column'} spacing={1}>
                     <FormHelperText>{ title }</FormHelperText>                 
                     <Stack direction={'row'} spacing = {0.5} alignItems={'center'}>
-                        <AccountCircleIcon sx={ {fontSize:{ xs: 60, md: 80 } }}/>
-                        <Stack direction={'column'} spacing={0.5}>
-                            <Typography variant='profile_name'>{ name }</Typography>
+                        <AccountCircleIcon sx={ {fontSize:{ xs: 50, md: 70 } }}/>
+                        <Stack direction={'column'} spacing={0.2}>
+                            <Typography variant='h4'>{ name }</Typography>
                             { subtitle && <Typography variant= 'button'>{ subtitle }</Typography>  }
                         </Stack>                      
                     </Stack>
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,14 +3,14 @@ import { BasicCard } from "../components/BasicCard"
 import { OrgJobPosts } from "./profile/organization/OrgJobPosts"
 import { Qualification } from "./profile/job_seeker/Qualifications"
 import { JobSeekerPosts} from "./profile/job_seeker/JobSeekerPosts"
+import { Messaging } from "../components/profile/Message/Messaging"
 
 
 export const Home = () => {
 
     return (
         <>
-        <Qualification/>
-        <JobSeekerPosts/>
+            <Messaging/>
         </>
     )
 }
