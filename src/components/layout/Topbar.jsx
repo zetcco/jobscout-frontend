acd9c41149a8c0678e7b9ae816b85a33d50ea208
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-01-28 14:21:04 +0530

    Topbar refactor

--- src/components/layout/Topbar.jsx
+++ src/components/layout/Topbar.jsx
@@ -1,4 +1,5 @@
-import { AppBar, Box, Toolbar, Typography } from "@mui/material"
+import { AccountCircle, ChatBubbleOutlineOutlined, DashboardCustomizeOutlined, NotificationsNoneOutlined, KeyboardArrowDownOutlined } from "@mui/icons-material"
+import { AppBar, Box, IconButton, Stack, Toolbar, Typography } from "@mui/material"
 
 export const Topbar = () => {
 
@@ -10,6 +11,25 @@ export const Topbar = () => {
             }}>
                 <Toolbar>
                     <Typography variant="h5" sx={{ flexGrow: 1, color: (theme) => theme.palette.common.white }}> JobScout </Typography>
+                    <Box sx={{ flexGrow: 1 }} />
+                    <Box sx={{ display: { xs: '12', md: 'flex' } }}>
+                        <IconButton size='large' color='inherit'>
+                            <ChatBubbleOutlineOutlined />
+                        </IconButton>
+                        <IconButton size='large' color='inherit'>
+                            <DashboardCustomizeOutlined />
+                        </IconButton>
+                        <IconButton size='large' color='inherit'>
+                            <NotificationsNoneOutlined/>
+                        </IconButton>
+                        <IconButton edge='end' aria-haspopup='true' color='inherit' >
+                        <Stack direction='row' spacing={0.5}>
+                            <AccountCircle size='large'/>
+                            <Typography>Thanis</Typography>
+                            <KeyboardArrowDownOutlined size='small'/>
+                        </Stack>
+                        </IconButton>
+                    </Box>
                 </Toolbar>
             </AppBar>
         </Box>
