f5da8dd38b0668299dad668100b5e53884235cd3
ANMSilva <nipunmadumal18@gmail.com>
2023-01-28 12:25:12 +0530

    ProfileWithFullNameSubtitleSmall created

---
+++ src/components/profile/ProfileWithFullNameSubtitleSmall.jsx
@@ -0,0 +1,18 @@
+import { Stack } from "@mui/system";
+import { FormHelperText ,Typography} from "@mui/material";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+export const ProfileWithFullNameSubtitle = ({title , name , subtitle}) => {
+    return ( 
+            <Stack direction={'column'} spacing={1}>
+                    <FormHelperText>{ title }</FormHelperText>                 
+                    <Stack direction={'row'} spacing = {0.5} alignItems={'center'}>
+                        <AccountCircleIcon sx={ {fontSize:{ xs: 60, md: 80 } }}/>
+                        <Stack direction={'column'} spacing={0.5}>
+                            <Typography variant='profile_name'>{ name }</Typography>
+                            { subtitle && <Typography variant= 'button'>{ subtitle }</Typography>  }
+                        </Stack>                      
+                    </Stack>
+            </Stack>
+     );
+}
