a169987e54873fd1d8ca77599b33bf692ad82f7c
Gimhan Mahela <gimhanmahela@gmail.com>
2023-01-28 13:20:25 +0530

    AddEducationalCard

--- src/routes/signup/Test/Test.jsx
+++ src/routes/signup/Test/Test.jsx
@@ -2,13 +2,16 @@ import { Grid } from '@mui/material'
 import React from 'react'
 import { EducationalCard } from '../../../components/profile/education/EducationalCard'
 import EducationQualificationForm from '../../../components/authentication/EducationQualificationForm'
+import { OrganizationProfileCreation } from '../organization/OrganizationProfileCreation'
 
 
 export const Test = () => {
     return (
       <Grid container>
         <Grid item xs={12}>
+          
           <EducationQualificationForm />
+          
         </Grid>
       </Grid>
     )
