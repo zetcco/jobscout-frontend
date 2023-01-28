2eba4c8462d877fa401b9c2cdf9bf2e30c6be15b
Gimhan Mahela <gimhanmahela@gmail.com>
2023-01-28 22:17:03 +0530

    Create AddCompanyForm

--- src/components/authentication/user/job_creator/AddCompanyForm.jsx
+++ src/components/authentication/user/job_creator/AddCompanyForm.jsx
@@ -1,7 +1,8 @@
-import { Button, TextField } from '@mui/material'
+import { Button, TextField, Box } from '@mui/material'
 import { Stack } from '@mui/system'
 import React from 'react'
 import { CenteredHeaderCard } from '../../../cards/CenteredHeaderCard'
+import SearchIcon from '@mui/icons-material/Search';
 
 
 
@@ -14,14 +15,22 @@ const AddCompanyForm = () => {
         title={" Add Your Company"} 
         footer={
             <Stack direction="row" spacing={2}>
-                <Button variant='outlined' sx={{ width: '100%' }}> Create New </Button>
-                <Button variant='contained' sx={{ width: '100%' }}>Continue</Button>
+                
+                  <Button variant='outlined' sx={{ width: '100%' }}> Create Your Company Profile </Button>
+                              
+                  <Button variant='contained' sx={{ width: '100%' }}>Continue</Button>
+                
+                
             </Stack>
       }>
             
-      <Stack spacing={2} sx={{ width: '100%' }}>
-        <TextField id="outlined-basic" label="Search Your Company" variant="outlined" />
-      </Stack>
+      
+        <Stack spacing={2} sx={{ width: '100%' }}> 
+          <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+            <SearchIcon fontSize='large' />
+            <TextField sx={{ width: '100%' }} id="outlined-basic" label="Search Your Company" variant="outlined" />
+          </Box>
+        </Stack>
       
       </CenteredHeaderCard>
     
