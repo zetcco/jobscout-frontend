b96fdebed35128e808b327b0a666dabe5fcd14ef
Gayesh Chamoda <npgayesh@gmail.com>
2023-01-30 00:04:16 +0530

    Change content of typography, modify sidebars

--- src/routes/blog/Blog.jsx
+++ src/routes/blog/Blog.jsx
@@ -8,12 +8,19 @@ function Blog() {
         <Stack direction="colomn" justifyContent="space-between" alignItems="stretch" spacing={5}>
             <SmallPanel 
               mainTitle={'Left'}
+              children = {[
+                <button variant = {'outlined'} fullWidth>Left Button</button>
+              ]}
             />
           <Stack>
+            <BlogPostSummary/>
             <BlogPostSummary/>
           </Stack>
           <SmallPanel 
              mainTitle={'Right'} 
+             children = {[
+              <button variant = {'outlined'} fullWidth>Right Button</button>
+            ]}
           />
         </Stack>
     </>
