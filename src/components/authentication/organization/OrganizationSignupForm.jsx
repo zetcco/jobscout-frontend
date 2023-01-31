95559b8fd21e8b27eea86459455dd86d66cf7f62
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-01-31 22:48:21 +0530

    Organization SIgnup form

--- src/components/authentication/organization/OrganizationSignupForm.jsx
+++ src/components/authentication/organization/OrganizationSignupForm.jsx
@@ -8,29 +8,37 @@ import Select from '@mui/material/Select';
 import { CenteredHeaderCard } from "../../cards/CenteredHeaderCard";
 import { DashedArea } from "../../input/DashedArea";
 import UploadIcon from '@mui/icons-material/FileUpload';
-import { Link as RouterLink } from "react-router-dom";
 import { Controller, useForm } from "react-hook-form";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCities, fetchCountries, fetchProvince, selectCities, selectCountries, selectProvince } from "../../../features/addressSlice";
 
 const OrganizationSignupForm = () => {
-    const [city, setCity] = React.useState('')
-    const [province, setProvince] = React.useState('')
-    const [country, setCountry] = React.useState('')
+    const {control, handleSubmit, formState: { errors }, watch }= useForm();
+    const dispatch = useDispatch();
 
-    const {control, handleSubmit, formState: { errors } }= useForm();
+    const watchCountry = watch("country");
+    const watchProvince = watch("province");
 
-    const handleCityChange = (event) => {
-        setCity(event.target.value);
-    };
-    const handleProvinceChange = (event) => {
-        setProvince(event.target.value);
-    };
-    const handleCountryChange = (event) => {
-        setCountry(event.target.value);
-    };
-    
+    const countries = useSelector(selectCountries);
+    const provice = useSelector(selectProvince);
+    const cities = useSelector(selectCities);
+
     const onSubmit=(data) =>{
         console.log(data)
     }
+
+    useEffect(() => {
+        dispatch(fetchCountries())
+    }, [dispatch])
+
+    useEffect(() => {
+        dispatch(fetchProvince(watchCountry))
+    }, [dispatch, watchCountry])
+
+    useEffect(() => {
+        dispatch(fetchCities(watchProvince))
+    }, [dispatch, watchProvince])
 
     return (
         <CenteredHeaderCard
@@ -42,163 +50,164 @@ const OrganizationSignupForm = () => {
                     <Grid container spacing={2}>
                         <Grid item xs={12}>
                             <Controller 
-                            name="companyName"
-                            rules={ { required: true } }
-                            control={control}
-                            defaultValue=""
-                            render={ ({field}) =>(
-                            <TextField
-                                {...field}
-                                label="Company Name"
-                                variant="outlined"
-                                placeholder="Enter your company name"
-                                fullWidth
-                                error={errors.companyName && true}
-                            />
-                            )}
-
+                                name="companyName"
+                                rules={ { required: true } }
+                                control={control}
+                                defaultValue=""
+                                render={ ({field}) =>(
+                                <TextField
+                                    {...field}
+                                    label="Company Name"
+                                    variant="outlined"
+                                    placeholder="Enter your company name"
+                                    fullWidth
+                                    error={errors.companyName && true}
+                                />)}
                            />
-
                         </Grid>
 
                         <Grid item xs={12}>
                             <Controller
-                            name="companyEmail"
-                            rules={ { required: true } }
-                            control={control}
-                            defaultValue=""
-                            render={ ({field}) =>( 
-                            <TextField
-                                {...field}
-                                label="Company Email"
-                                variant="outlined"
-                                placeholder="Enter your company Email"
-                                fullWidth
-                                error={errors.companyEmail && true}
+                                name="companyEmail"
+                                rules={ { required: true } }
+                                control={control}
+                                defaultValue=""
+                                render={ ({field}) =>( 
+                                <TextField
+                                    {...field}
+                                    label="Company Email"
+                                    variant="outlined"
+                                    placeholder="Enter your company Email"
+                                    fullWidth
+                                    error={errors.companyEmail && true}
+                                />)}
                             />
-                            )}
-
-                            />
-
                         </Grid>
 
                         <Grid item xs={6} lg={4}>
-                            <Controller
-                            name="steetNumber"
-                            rules={ {required: true} }
-                            control={control}
-                            defaultValue=""
-                            render={ ({field}) =>(
-                            <TextField 
-                                {...field}
-                                label="Steet Number" 
-                                variant="outlined"
-                                placeholder = "Your street number"
-                                fullWidth 
-                                error={errors.steetNumber && true}
-                            />) }
-                            />
-                            
+                            <FormControl fullWidth error={errors.country && true}>
+                                <InputLabel id="Org-registration-country-select-label">Country</InputLabel>
+                                <Controller
+                                    name="country"
+                                    rules={{ required: true }}
+                                    control={control}
+                                    defaultValue=""
+                                    render={ ({ field }) => (
+                                        <Select
+                                            {...field}
+                                            labelId="Org-registration-country-select-label"
+                                            label="Country"
+                                        >
+                                            { 
+                                                countries.map( (country, index) => (
+                                                    <MenuItem value={country} key={index}>{country}</MenuItem>
+                                                ))
+                                            }
+                                        </Select>
+                                    )}
+                                />
+                            </FormControl>
                         </Grid>
 
                         <Grid item xs={6} lg={4}>
-                            <FormControl fullWidth>
-                                <InputLabel id="Org-registration-street-select-label">Street</InputLabel>
-                                <Select
-                                    labelId="Org-registration-town-select-label"
-                                    id="Org-registration-street-select"
-                                    value={province}
-                                    label="Street"
-                                    onChange={handleProvinceChange}
-                                >
-                                    <MenuItem value="">
-                                        <em>None</em>
-                                    </MenuItem>
-                                    <MenuItem value={10}>Ten</MenuItem>
-                                    <MenuItem value={20}>Twenty</MenuItem>
-                                    <MenuItem value={30}>Thirty</MenuItem>
-                                </Select>
+                            <FormControl fullWidth error={errors.province && true}>
+                                <InputLabel id="Org-registration-province-select-label">Province</InputLabel>
+                                <Controller
+                                    name="province"
+                                    rules={{ required: true }}
+                                    control={control}
+                                    defaultValue=""
+                                    render={ ({ field }) => (
+                                        <Select
+                                            {...field}
+                                            labelId="Org-registration-province-select-label"
+                                            label="Province"
+                                        >
+                                            { 
+                                                provice.map( (provice, index) => (
+                                                    <MenuItem value={provice} key={index}>{provice}</MenuItem>
+                                                ))
+                                            }
+                                        </Select>
+                                    )}
+                                />
+                            </FormControl>
+                        </Grid>
+
+                        <Grid item xs={6} lg={4}>
+                            <FormControl fullWidth error={errors.city && true}>
+                                <InputLabel id="Org-registration-city-select-label">City</InputLabel>
+                                <Controller
+                                    name="city"
+                                    rules={{ required: true }}
+                                    control={control}
+                                    defaultValue=""
+                                    render={ ({ field }) => (
+                                        <Select
+                                            {...field}
+                                            labelId="Org-registration-city-select-label"
+                                            label="City"
+                                        >
+                                            { 
+                                                cities.map( (city, index) => (
+                                                    <MenuItem value={city} key={index}>{city}</MenuItem>
+                                                ))
+                                            }
+                                        </Select>
+                                    )}
+                                />
                             </FormControl>
                         </Grid>
 
                         <Grid item  xs={6} lg={4}>
-                            <FormControl fullWidth>
-                                <InputLabel id="Org-registration-town-select-label">Town</InputLabel>
-                                <Select
-                                    labelId="Org-registration-town-select-label"
-                                    id="Org-registration-town-select"
-                                    value={country}
-                                    label="Town"
-                                    onChange={handleCountryChange}
-                                >
-                                    <MenuItem value="">
-                                        <em>None</em>
-                                    </MenuItem>
-                                    <MenuItem value={10}>Ten</MenuItem>
-                                    <MenuItem value={20}>Twenty</MenuItem>
-                                    <MenuItem value={30}>Thirty</MenuItem>
-                                </Select>
-                            </FormControl>
+                            <Controller
+                                name="town"
+                                control={control}
+                                defaultValue=""
+                                render={ ({field}) =>(
+                                <TextField 
+                                    {...field}
+                                    label="Town" 
+                                    variant="outlined"
+                                    placeholder = "Your Town"
+                                    fullWidth 
+                                    error={errors.town && true}
+                                />)}
+                            />
                         </Grid>
 
                         <Grid item xs={6} lg={4}>
-                            <FormControl fullWidth>
-                                <InputLabel id="Org-registration-city-select-label">City</InputLabel>
-                                <Select
-                                    labelId="Org-registration-city-select-label"
-                                    id="Org-registration-city-select"
-                                    value={city}
-                                    label="City"
-                                    onChange={handleCityChange}
-                                >
-                                    <MenuItem value="">
-                                        <em>None</em>
-                                    </MenuItem>
-                                    <MenuItem value={10}>Ten</MenuItem>
-                                    <MenuItem value={20}>Twenty</MenuItem>
-                                    <MenuItem value={30}>Thirty</MenuItem>
-                                </Select>
-                            </FormControl>
+                            <Controller
+                                name="street"
+                                control={control}
+                                defaultValue=""
+                                render={ ({field}) =>(
+                                <TextField 
+                                    {...field}
+                                    label="Steet" 
+                                    variant="outlined"
+                                    placeholder = "Your Street"
+                                    fullWidth 
+                                    error={errors.street && true}
+                                />)}
+                            />
                         </Grid>
 
                         <Grid item xs={6} lg={4}>
-                            <FormControl fullWidth>
-                                <InputLabel id="Org-registration-province-select-label">Province</InputLabel>
-                                <Select
-                                    labelId="Org-registration-city-select-label"
-                                    id="Org-registration-city-select"
-                                    value={province}
-                                    label="Province"
-                                    onChange={handleProvinceChange}
-                                >
-                                    <MenuItem value="">
-                                        <em>None</em>
-                                    </MenuItem>
-                                    <MenuItem value={10}>Ten</MenuItem>
-                                    <MenuItem value={20}>Twenty</MenuItem>
-                                    <MenuItem value={30}>Thirty</MenuItem>
-                                </Select>
-                            </FormControl>
-                        </Grid>
-
-                        <Grid item xs={6} lg={4}>
-                            <FormControl fullWidth>
-                                <InputLabel id="Org-registration-country-select-label">Country</InputLabel>
-                                <Select
-                                    labelId="Org-registration-country-select-label"
-                                    id="Org-registration-country-select"
-                                    value={country}
-                                    label="Country"
-                                    onChange={handleCountryChange}
-                                >
-                                    <MenuItem value="">
-                                        <em>None</em>
-                                    </MenuItem>
-                                    <MenuItem value={10}>Ten</MenuItem>
-                                    <MenuItem value={20}>Twenty</MenuItem>
-                                    <MenuItem value={30}>Thirty</MenuItem>
-                                </Select>
-                            </FormControl>
+                            <Controller
+                                name="steetNumber"
+                                control={control}
+                                defaultValue=""
+                                render={ ({field}) =>(
+                                <TextField 
+                                    {...field}
+                                    label="Steet Number" 
+                                    variant="outlined"
+                                    placeholder = "Your street number"
+                                    fullWidth 
+                                    error={errors.steetNumber && true}
+                                />)}
+                            />
                         </Grid>
 
                         <Grid item xs={12}>
@@ -213,7 +222,7 @@ const OrganizationSignupForm = () => {
                         </Grid>
                     
                      </Grid>
-                    </form>
+                </form>
                 </Stack>
         </CenteredHeaderCard>
 
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from '../features/usersSlice';
 import authReducer from '../features/authSlice';
+import addressReducer from "../features/addressSlice";
 
 export default configureStore({
     reducer: {
         auth: authReducer,
-        users: userReducer
+        users: userReducer,
+        address: addressReducer
     }
 })
