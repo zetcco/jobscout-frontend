8afd14cd80d930e1c9b855f0febae02276b0c78e
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-01-31 19:13:19 +0530

    Redirect login page

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,7 +1,17 @@
 import { Grid } from "@mui/material"
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
 import SigninForm from "../components/authentication/SigninForm"
+import { selectAuthUser } from "../features/authSlice";
 
 export const Login = () => {
+
+    const authUser = useSelector(selectAuthUser);
+
+    if (authUser)
+        return (<Navigate to={"/home"} replace/>)
+
     return (
     <Grid container>
         <Grid item xs={12}>
