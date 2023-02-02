64f844dbf00be8e916ef52c757af13ea3a657fde
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-02-02 22:32:21 +0530

    Uncompleted authentication

--- src/routes/signup/users/job_seeker/SeekerSignup.jsx
+++ src/routes/signup/users/job_seeker/SeekerSignup.jsx
@@ -1,8 +1,16 @@
 import { Grid } from '@mui/material'
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router';
 import SeekerSignupForm from '../../../../components/authentication/user/job_seeker/SeekerSignupForm'
+import { selectAuthUser } from '../../../../features/authSlice';
 
 export const SeekerSignup = () => {
+  const authUser = useSelector(selectAuthUser);
+
+  if (authUser)
+      return (<Navigate to={"/signup/user/seeker/profile/skills"} replace/>)
+
   return (
     <Grid container>
       <Grid item xs={12}>
