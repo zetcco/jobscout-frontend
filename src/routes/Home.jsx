f16f598ae2eb83d3f7a92af852713330338664bd
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-02-02 23:14:16 +0530

    authUser for Home component

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,10 @@
+import { useSelector } from "react-redux"
 import { Messaging } from "../components/profile/Message/Messaging"
+import { selectAuthUser } from "../features/authSlice"
 
 export const Home = () => {
+    
+    const authUser = useSelector(selectAuthUser)
 
     return (
         <>
