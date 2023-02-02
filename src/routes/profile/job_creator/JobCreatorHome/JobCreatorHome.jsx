88c2f5620bbe3848cbb2ae6da750a46a3ef3b115
ANMSilva <nipunmadumal18@gmail.com>
2023-02-02 22:23:49 +0530

    JobSeekerHome component modified

--- src/routes/profile/job_creator/JobCreatorHome/JobCreatorHome.jsx
+++ src/routes/profile/job_creator/JobCreatorHome/JobCreatorHome.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { BasicCard } from '../../../../components/cards/BasicCard'
 import { Stack } from '@mui/system'
 import SmallPanel from '../../../../components/SmallPanel'
-import { Button , Grid } from '@mui/material'
+import {Grid, Typography } from '@mui/material'
 import { JobCreatorHomeCards } from './JobCreatorHomeCards'
+import { SelectableCard } from '../../../../components/cards/SelectableCard'
 import AddBoxIcon from '@mui/icons-material/AddBox';
 import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+import WorkIcon from '@mui/icons-material/Work';
 
 export const JobCreatorHome = () => {
   return (
@@ -15,42 +17,71 @@ export const JobCreatorHome = () => {
                     <SmallPanel
                         mainTitle = {'Actions'}
                     >
-                            <Stack direction={'row'} spacing = {2}>
-                                <Button variant = 'outlined' sx = {{height:'100px' , fontSize:'25px'}} startIcon = {<AddBoxIcon sx = {{width:'30px', height: '30px'}}/>} fullWidth >Create</Button>
-                                <Button variant = 'outlined' sx = {{height:'100px' , fontSize:'25px'}} startIcon = {<ManageAccountsIcon sx = {{width:'30px', height: '30px'}}/>} fullWidth >Manage</Button>
+                            <Stack direction={'row'} spacing = {2} sx = {{justifyContent:'space-evenly'}}>
+                                <SelectableCard
+                                    sx  = {{flexGrow:1}}
+                                    title = { 
+                                            <Stack direction = {'column'} alignItems = {'center'} justifyContent = {'center'} spacing = {1}>
+                                                <AddBoxIcon sx = {{height:'30px' , width:'30px'}}/>
+                                                <Typography variant = 'h5'>CREATE</Typography>
+                                             </Stack> }
+                                    />
+                                <SelectableCard
+                                    sx  = {{flexGrow:1}}
+                                    title = { 
+                                            <Stack direction = {'column'} alignItems = {'center'} justifyContent = {'center'} spacing = {1}>
+                                                <ManageAccountsIcon sx = {{height:'30px' , width:'30px'}}/>
+                                                <Typography variant = 'h5'>MANAGE</Typography>
+                                            </Stack> }
+                                    />
+
+                                <SelectableCard
+                                    sx  = {{flexGrow:1}}
+                                    title = { 
+                                            <Stack direction = {'column'} alignItems = {'center'} justifyContent = {'center'} spacing = {1}>
+                                                <WorkIcon sx = {{height:'30px' , width:'30px'}}/>
+                                                <Typography variant = 'h5'>VACANCIES</Typography>
+                                            </Stack> }
+                                    />
                             </Stack>
                     </SmallPanel> 
                 </Stack>
+               
                 <Stack direction = {'row'} spacing = {2}>
-                    <Grid container spacing={2}>
-                    <Grid item xs = {4}>
-                        <Stack flexGrow={1}>
-                            <JobCreatorHomeCards
-                                title = {'No of job posts'}
-                                count = {6}
-                                color = {'blue'}
-                            />
-                        </Stack>
-                    </Grid>
-                    <Grid item xs = {4}>
-                        <Stack flexGrow={1}>
-                            <JobCreatorHomeCards
-                                title = {'No of activated posts'}
-                                count = {10}
-                                color = {'green'}
-                            />
-                        </Stack>
-                    </Grid>
-                    <Grid item xs = {4}>
-                        <Stack flexGrow={1}>
-                            <JobCreatorHomeCards
-                                title = {'No of deactivated posts'}
-                                count = {2}
-                                color = {'red'}
-                            />
-                        </Stack>
-                    </Grid></Grid>
+                    <Grid container spacing={2} sx = {{alignItems:'stretch'}}>
+                        <Grid item xs = {12} md = {4}>
+                            <Stack flexGrow={1}>
+                                <JobCreatorHomeCards
+                                    title = {'POSTS'}
+                                    subtitle = {'No of job posts'}
+                                    count = {6}
+                                    color = {'blue'}
+                                />
+                            </Stack>
+                        </Grid>
+                        <Grid item xs = {12} md = {4}>
+                            <Stack flexGrow={1}>
+                                <JobCreatorHomeCards
+                                    title = {'ACTIVATED POSTS'}
+                                    subtitle = {'No of activated posts'}
+                                    count = {10}
+                                    color = {'green'}
+                                />
+                            </Stack>
+                        </Grid>
+                        <Grid item xs = {12} md = {4}>
+                            <Stack flexGrow={1}>
+                                <JobCreatorHomeCards
+                                    title = {'DEACTIVATED POSTS'}
+                                    subtitle = {'No of deactivated posts'}
+                                    count = {2}
+                                    color = {'red'}
+                                />
+                            </Stack>
+                        </Grid>
+                    </Grid>           
                 </Stack>
+
             </Stack>
         </BasicCard>
   )
