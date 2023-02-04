0d843ad472337482061541135472d41ed3ecb7a0
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-02-04 17:05:15 +0530

    NavigationLayout transition animation

--- src/components/layout/NavigationLayout.jsx
+++ src/components/layout/NavigationLayout.jsx
@@ -1,8 +1,11 @@
 import { Box, Toolbar } from "@mui/material";
-import { Outlet } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion"
+import { Outlet, useLocation } from "react-router-dom";
 import { Topbar } from "./Topbar";
 
 export const NavigationLayout = () => {
+
+  const { pathname } = useLocation();
 
     return (
         <>
@@ -10,7 +13,15 @@ export const NavigationLayout = () => {
             <Box>
                 <Toolbar/>
                 <Box sx={{ mx: { md: '100px', lg: '250px' }, mt: 4 }}>
+                            <motion.div
+          key={pathname}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.4 }}
+        >
                     <Outlet/>
+                          </motion.div>
                 </Box>
             </Box>
         </>
