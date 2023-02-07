323056173b6c3a93e69ebd3f8cf9009ecc8e6818
Niroshan Sanjeewa <sanjeewa11138@usci.ruh.ac.lk>
2023-02-07 17:26:52 +0530

    remove dubble button

--- src/routes/blog/Blog.jsx
+++ src/routes/blog/Blog.jsx
@@ -2,7 +2,6 @@ import { Stack } from "@mui/system"
 import {Button } from '@mui/material'
 import BlogPostSummary from "../../components/blog/BlogPostSummary"
 import SmallPanel from "../../components/SmallPanel"
-import {Button } from '@mui/material'
 
 function Blog() {
   return (
