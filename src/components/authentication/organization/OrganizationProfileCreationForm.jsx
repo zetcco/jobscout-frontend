5b9a91011ebce6f1782f3b3c49c701dbb2f78b8e
Thanis <77865201+thanis04@users.noreply.github.com>
2023-02-08 07:33:59 +0530

    format chaged for easy study

--- src/components/authentication/organization/OrganizationProfileCreationForm.jsx
+++ src/components/authentication/organization/OrganizationProfileCreationForm.jsx
@@ -1,31 +1,45 @@
-import { Box, Button, Stack, Typography } from '@mui/material'
-import React from 'react'
-import { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { selectAuthLoading, uploadBusinessRegistration } from '../../../features/authSlice'
-import { CenteredHeaderCard } from '../../cards/CenteredHeaderCard'
-import { DashedArea } from '../../input/DashedArea'
-import { UploadArea } from '../../input/UploadArea'
+import { Box, Button, Stack, Typography } from '@mui/material';
+import React from 'react';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  selectAuthLoading,
+  uploadBusinessRegistration,
+} from '../../../features/authSlice';
+import { CenteredHeaderCard } from '../../cards/CenteredHeaderCard';
+import { UploadArea } from '../../input/UploadArea';
 
 export const OrganizationProfileCreationForm = () => {
   const dispatch = useDispatch();
   const loading = useSelector(selectAuthLoading);
-  const [ file, setFile ] = useState();
+  const [file, setFile] = useState();
 
-  console.log(file)
+  console.log(file);
 
   return (
-    <CenteredHeaderCard title={"Create your Profile"} >
-        <Stack spacing={2} sx={{ width: '100%' }}>
-            <UploadArea text={"Click here to Upload Logo"} handleFile={(data) => { setFile(data) }}/>
-            { file && (
-              <Typography>{ file.name }</Typography>
-            ) }
-            <Stack spacing={2} direction="row">
-              <Button variant='outlined' sx={{ width: '100%' }}>Skip</Button>
-              <Button disabled={loading && true} variant='contained' onClick={() => dispatch(uploadBusinessRegistration(file))} sx={{ width: '100%' }}>Continue</Button>
-          </Stack>
+    <CenteredHeaderCard title={'Create your Profile'}>
+      <Stack spacing={2} sx={{ width: '100%' }}>
+        <UploadArea
+          text={'Click here to Upload Logo'}
+          handleFile={(data) => {
+            setFile(data);
+          }}
+        />
+        {file && <Typography>{file.name}</Typography>}
+        <Stack spacing={2} direction='row'>
+          <Button variant='outlined' sx={{ width: '100%' }}>
+            Skip
+          </Button>
+          <Button
+            disabled={loading && true}
+            variant='contained'
+            onClick={() => dispatch(uploadBusinessRegistration(file))}
+            sx={{ width: '100%' }}
+          >
+            Continue
+          </Button>
         </Stack>
+      </Stack>
     </CenteredHeaderCard>
-  )
-}
+  );
+};
