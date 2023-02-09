be956d6282ed97390b0e016d72fb7291e9366089
Niroshan Sanjeewa <sanjeewa11138@usci.ruh.ac.lk>
2023-02-09 20:01:07 +0530

    add -button-mangepost

--- src/components/job_postings/manage/ManageJobPost.jsx
+++ src/components/job_postings/manage/ManageJobPost.jsx
@@ -1,16 +1,25 @@
-import { Grid , Stack } from '@mui/material'
+import { Grid , Stack , Button} from '@mui/material'
 import React from 'react'
+import SmallPanel from '../../SmallPanel'
 import { JobApplication } from './JobApplication'
+
 
 export const ManageJobPost = () => {
   return (
-    <Stack spacing={2}>
-      <JobApplication />
-      <JobApplication />
-      <JobApplication />
-      <JobApplication />
+    <Stack direction={'row'} spacing={2}>
+                <Stack spacing={2}>
+                  <JobApplication />
+                  <JobApplication />
+                  <JobApplication />
+                  <JobApplication />
+                </Stack>
+                <Stack direction={''}>
+                  <SmallPanel children={'children'}>
+                           <Button variant="outlined">Button</Button>
+                   </SmallPanel>
+                </Stack>
+
     </Stack>
-      
 
         )
 }
