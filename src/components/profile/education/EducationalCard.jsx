4bcb1c998d6d5302e607f6e7fe7a2fdd91179604
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-02-15 17:53:41 +0530

    Card with close button

--- src/components/profile/education/EducationalCard.jsx
+++ src/components/profile/education/EducationalCard.jsx
@@ -1,29 +1,16 @@
 import React from "react";
-import { Box, Grid, Typography} from "@mui/material"
-import { BasicCard } from "../../cards/BasicCard";
-import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import { Box, Typography} from "@mui/material"
+import { CardWithCloseButton } from "components/CardWithCloseButton";
 
 export const EducationalCard = ({DegreeName,Institution,Duration}) => {
     return (
-        <BasicCard>
-            <Grid
-                container
-                direction="row"
-                justifyContent="space-between"
-                alignItems="center"
-                >
-                    <Grid item>
-                        <Box>
-                            <Typography variant="h6_bold">{ DegreeName }</Typography>
-                            <Typography variant="h6">{ Institution }</Typography>
-                            <Typography variant="h6">{ Duration }</Typography>
-                        </Box>
-                    </Grid>
-                    <Grid item>
-                        <HighlightOffIcon fontSize="large"/>
-                    </Grid>
-            </Grid>
-        </BasicCard>
+        <CardWithCloseButton>
+            <Box>
+                <Typography variant="h6_bold">{ DegreeName }</Typography>
+                <Typography variant="h6">{ Institution }</Typography>
+                <Typography variant="h6">{ Duration }</Typography>
+            </Box>
+        </CardWithCloseButton>
     )
 }
     
