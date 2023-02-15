e763f8c5430de5b3f44393504bc52ad40ed1fd01
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-02-15 17:53:54 +0530

    Fetch Organizations by searching

--- src/components/authentication/user/job_creator/AddCompanyForm.jsx
+++ src/components/authentication/user/job_creator/AddCompanyForm.jsx
@@ -1,11 +1,32 @@
-import { Button, TextField, Box, InputAdornment } from '@mui/material'
+import { Button, TextField, Box, InputAdornment, Autocomplete, Typography, Avatar } from '@mui/material'
 import { Stack } from '@mui/system'
-import React from 'react'
+import { useEffect, useState } from 'react'
 import { CenteredHeaderCard } from '../../../cards/CenteredHeaderCard'
 import SearchIcon from '@mui/icons-material/Search';
 import { RouterLink } from '../../../RouterLink';
+import axios from "axios"
+import { AvatarWithInitials } from 'components/AvatarWithInitials';
+import { CardWithCloseButton } from 'components/CardWithCloseButton';
 
 const AddCompanyForm = () => {
+
+  const [value, setValue] = useState(null);
+  const [inputValue, setInputValue] = useState('');
+  const [options, setOptions] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (inputValue !== '') {
+      const fetchData = async () => { 
+        setLoading(true)
+        const response = await axios.get(`/organization/search?q=${inputValue}&limit=10&offset=0`)
+        setOptions(response.data)
+        setLoading(false)
+      }
+      fetchData().catch(console.error)
+    }
+  }, [inputValue])
+
   return (
     <Stack spacing={2} sx={{ width: '100%' }}>
     
@@ -28,28 +49,69 @@ const AddCompanyForm = () => {
               <Button variant='contained' sx={{ width: '100%' }} disabled>Continue</Button>
             </RouterLink>
           </Box>
-      </Stack>     
-                
-            
+          </Stack>     
       }>
-            
-      
-        <Stack spacing={2} sx={{ width: '100%' }}>
 
-        <TextField
-          id="input-with-icon-textfield"
-          label="Search Your Company"
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <SearchIcon />
-              </InputAdornment>
-            ),
+      <Stack spacing={4} sx={{ width: '100%' }}>
+        <Autocomplete
+          value={value}
+          inputValue={inputValue}
+          onChange={(e, value) => { setValue(value) }}
+          onInputChange={(e, value) => { setInputValue(value) }}
+          loading={loading}
+          filterOptions={(x) => x}
+          options={options}
+          getOptionLabel={(option) => typeof option === 'string' ? option : option.displayName }
+          isOptionEqualToValue={(option, value) => option.id === value.id}
+          renderInput={(params) => 
+            <TextField 
+              {...params}
+              label="Search Your Company"
+              InputProps={{
+                ...params.InputProps,
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SearchIcon />
+                    {params.InputProps.startAdornment}
+                  </InputAdornment>
+                ),
+              }}
+            />
+          }
+          renderOption={(props, option) => {
+              return (
+                <li {...props}>
+                  <Stack direction={"row"} spacing={1}>
+                    {
+                        option.displayPicture ? (
+                            <Avatar src={option.displayPicture} sx={{ width: 24, height: 24 }}/>
+                        ) : (
+                            <Avatar sx={{ width: 24, height: 24 }}>{ option.displayName && (Array.from(option.displayName)[0]) }</Avatar>
+                        )
+                    }
+                    <Typography>{option.displayName}</Typography>
+                  </Stack>
+                </li>
+              )
           }}
-          variant="outlined"
         />
-        </Stack>
-      
+
+        {
+          value && (
+            <CardWithCloseButton onClose={() => setValue(null)}>
+              <Stack direction={"row"} alignItems="center" spacing={2}>
+                <AvatarWithInitials src={value.displayPicture} name={value.displayName} size={124}/>
+                <Stack direction={"column"}>
+                  <Typography variant='profile_name'>{value.displayName}</Typography>
+                  <Typography variant='h6'>{value.email}</Typography>
+                  <Typography variant='subtitle1'>{value.address?.city + ", " + value.address?.province + ", " + value.address?.country}</Typography>
+                </Stack>
+              </Stack>
+            </CardWithCloseButton>
+          )
+        }
+      </Stack>
+        
       </CenteredHeaderCard>
     
     </Stack>
