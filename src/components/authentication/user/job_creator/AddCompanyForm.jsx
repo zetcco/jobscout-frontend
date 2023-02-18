0a5575f656a1bb9c1e528d4f6be5b478db451371
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-02-18 16:52:39 +0530

    Send request to join for an organization

--- src/components/authentication/user/job_creator/AddCompanyForm.jsx
+++ src/components/authentication/user/job_creator/AddCompanyForm.jsx
@@ -1,4 +1,4 @@
-import { Button, TextField, Box, InputAdornment, Autocomplete, Typography, Avatar } from '@mui/material'
+import { Button, TextField, Box, InputAdornment, Autocomplete, Typography, Avatar, Alert, AlertTitle } from '@mui/material'
 import { Stack } from '@mui/system'
 import { useEffect, useState } from 'react'
 import { CenteredHeaderCard } from '../../../cards/CenteredHeaderCard'
@@ -7,58 +7,77 @@ import { RouterLink } from '../../../RouterLink';
 import axios from "axios"
 import { AvatarWithInitials } from 'components/AvatarWithInitials';
 import { CardWithCloseButton } from 'components/CardWithCloseButton';
+import { useSelector } from 'react-redux';
+import { selectAuthUserToken } from 'features/authSlice';
+import { useNavigate } from 'react-router-dom';
 
 const AddCompanyForm = () => {
 
   const [value, setValue] = useState(null);
   const [inputValue, setInputValue] = useState('');
   const [options, setOptions] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [fetchOrgsLoading, setFetchOrgsLoading] = useState(false);
 
+  const [error, setError] = useState(null);
+  const [submitOrgLoading, setSubmitOrgsLoading] = useState(false);
+
+  const authToken = useSelector(selectAuthUserToken);
+
+  const navigate = useNavigate();
+
+  // Request for setting an organization
+  const setOrganization = () => {
+    /* TODO: Move this function to somewhere more appropiate, maybe an Hook? */
+    const setOrganizationReq = async (organizationId) => {
+      setSubmitOrgsLoading(true);
+      axios.post(`/jobcreator/request-organization`,
+        { id: organizationId },
+        { headers: { "Authorization": `Bearer ${authToken}` }}
+      ).then((response) => {
+        setSubmitOrgsLoading(false);
+        navigate("/home")
+      }).catch((error) => {
+        setError(error.response.data)
+        setSubmitOrgsLoading(false);
+      })
+    }
+    setOrganizationReq(value.id)
+  }
+
+  // Fetch companies by searching
   useEffect(() => {
     if (inputValue !== '') {
-      const fetchData = async () => { 
-        setLoading(true)
-        const response = await axios.get(`/organization/search?q=${inputValue}&limit=10&offset=0`)
+      axios.get(`/organization/search?q=${inputValue}&limit=10&offset=0`)
+      .then((response) => {
         setOptions(response.data)
-        setLoading(false)
-      }
-      fetchData().catch(console.error)
+        setFetchOrgsLoading(false)
+      }).catch((error) => {
+        console.log(error)
+        setFetchOrgsLoading(false)
+      })
     }
   }, [inputValue])
 
   return (
     <Stack spacing={2} sx={{ width: '100%' }}>
     
-      <CenteredHeaderCard
-        title={" Add Your Company"} 
-        footer={
-          <Stack direction="row" spacing={2}>
-          <Box sx={{ width: '100%' }}>
-            <RouterLink to="/signup/user/dp">
-              <Button variant='outlined' sx={{ width: '100%' }}>Skip</Button>
-            </RouterLink>
-          </Box>
-          <Box sx={{ width: '100%' }}>
-            <RouterLink to="/signup/organization/account">
-              <Button variant='outlined' sx={{ width: '100%' }}>Create Your Company</Button>
-            </RouterLink>
-          </Box>
-          <Box sx={{ width: '100%' }}>
-            <RouterLink to="/signup/user/dp">
-              <Button variant='contained' sx={{ width: '100%' }} disabled>Continue</Button>
-            </RouterLink>
-          </Box>
-          </Stack>     
-      }>
-
+      <CenteredHeaderCard title={" Add Your Company"}>
       <Stack spacing={4} sx={{ width: '100%' }}>
+        {
+            error && 
+            (
+                <Alert severity="error">
+                    <AlertTitle>Error</AlertTitle>
+                    <strong>{error.message}</strong>
+                </Alert>
+            )
+        }
         <Autocomplete
           value={value}
           inputValue={inputValue}
           onChange={(e, value) => { setValue(value) }}
           onInputChange={(e, value) => { setInputValue(value) }}
-          loading={loading}
+          loading={fetchOrgsLoading}
           filterOptions={(x) => x}
           options={options}
           getOptionLabel={(option) => typeof option === 'string' ? option : option.displayName }
@@ -109,7 +128,22 @@ const AddCompanyForm = () => {
               </Stack>
             </CardWithCloseButton>
           )
-        }
+        }          
+        <Stack direction="row" spacing={2}>
+          <Box sx={{ width: '100%' }}>
+            <RouterLink to="/signup/user/dp">
+              <Button variant='outlined' sx={{ width: '100%' }}>Skip</Button>
+            </RouterLink>
+          </Box>
+          <Box sx={{ width: '100%' }}>
+            <RouterLink to="/signup/organization/account">
+              <Button variant='outlined' sx={{ width: '100%' }}>Create Your Company</Button>
+            </RouterLink>
+          </Box>
+          <Box sx={{ width: '100%' }}>
+            <Button variant='contained' sx={{ width: '100%' }} onClick={setOrganization} disabled={submitOrgLoading || value == null}>Continue</Button>
+          </Box>
+          </Stack>     
       </Stack>
         
       </CenteredHeaderCard>
@@ -117,5 +151,6 @@ const AddCompanyForm = () => {
     </Stack>
   )
 }
+
 
 export default AddCompanyForm
