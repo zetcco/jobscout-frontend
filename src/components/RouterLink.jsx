364a823b867a1c92fca1e9e4faf5edad1154d158
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-02-20 19:32:11 +0530

    Prettier test

--- src/components/RouterLink.jsx
+++ src/components/RouterLink.jsx
@@ -1,17 +1,15 @@
-import { styled } from '@mui/material'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { styled } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const StyledLink = styled(Link)(({ theme }) => ({
-  textDecoration: 'none',
-  color: 'inherit',
-  '&:active, &:visited': {
-    color: 'inherit'
-  },
-}))
+    textDecoration: "none",
+    color: "inherit",
+    "&:active, &:visited": {
+        color: "inherit",
+    },
+}));
 
 export const RouterLink = ({ to, children }) => {
-  return (
-    <StyledLink to={to}>{ children }</StyledLink>
-  )
-}
+    return <StyledLink to={to}>{children}</StyledLink>;
+};
