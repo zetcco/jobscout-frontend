926e30c188a75eae9dc4eab0e3159c8f67f3ab17
ANMSilva <nipunmadumal18@gmail.com>
2023-02-27 15:13:24 +0530

    FilterPosts's filter by fields functionality added and modified

--- src/components/job_postings/posts/FilterPosts.jsx
+++ src/components/job_postings/posts/FilterPosts.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState , useEffect } from "react";
 import {Stack} from '@mui/material';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import axios from "axios";
+import { selectAuthUserToken } from "features/authSlice";
+import { useSelector } from "react-redux";
 
-const FilterPosts = () => {
+const FilterPosts = () => { 
+
+    const [categories , setCategories] = useState([]);
+    const [category , setCategory] = useState('');
+    const token = useSelector(selectAuthUserToken)
+    useEffect(() => {
+        axios.get('/category/' , {
+            headers:{
+                Authorization: `Bearer ${token}`
+            }
+        })
+        .then(cat =>{
+            console.log(cat)
+            setCategories(cat.data)
+        })
+        .catch(err =>{
+            console.log(err)
+        })
+    } , [])
+
     return ( 
 
                         <Stack direction={'column'} spacing={4}>
@@ -14,14 +36,14 @@ const FilterPosts = () => {
                                     <Select
                                         labelId="Small-panel-filter-by-fields-label"
                                         id="Small-panel-filter-by-fields"
+                                        value = {category}
+                                        onChange = {(e) => setCategory(e.target.value)}
                                         label="Fileds"      
                                     >
-                                        <MenuItem value="">
-                                            <em>None</em>
-                                        </MenuItem>
-                                        <MenuItem value={10}>Ten</MenuItem>
-                                        <MenuItem value={20}>Twenty</MenuItem>
-                                        <MenuItem value={30}>Thirty</MenuItem>
+                                        {
+                                            categories.map((category) =>
+                                            <MenuItem value = {category.id}>{category.name}</MenuItem>)
+                                        }
                                     </Select>
                                 </FormControl>
                                 <FormControl>
@@ -32,12 +54,7 @@ const FilterPosts = () => {
                                         label="City"
     
                                     >
-                                        <MenuItem value="">
-                                            <em>None</em>
-                                        </MenuItem>
-                                        <MenuItem value={10}>Ten</MenuItem>
-                                        <MenuItem value={20}>Twenty</MenuItem>
-                                        <MenuItem value={30}>Thirty</MenuItem>
+                            
                                     </Select>
                                 </FormControl>
                                 <FormControl>
@@ -47,16 +64,11 @@ const FilterPosts = () => {
                                         id="Org-registration-city-select"
                                         label="City"
                                  >
-                                        <MenuItem value="">
-                                            <em>None</em>
-                                        </MenuItem>
-                                        <MenuItem value={10}>Ten</MenuItem>
-                                        <MenuItem value={20}>Twenty</MenuItem>
-                                        <MenuItem value={30}>Thirty</MenuItem>
+                                       
                                     </Select>
                             </FormControl>
                     </Stack>
      );
-}
+}           
  
 export default FilterPosts;
