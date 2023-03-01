92bf51953540c2d115c0f6c2c3bd230d906d927a
Thanis <77865201+thanis04@users.noreply.github.com>
2023-03-01 21:36:23 +0530

    try catch block use for get url

--- src/components/authentication/user/job_seeker/AddSkillsForm.jsx
+++ src/components/authentication/user/job_seeker/AddSkillsForm.jsx
@@ -1,52 +1,53 @@
-import { Button, Chip, MenuItem, Select } from "@mui/material"
-import { Box, Stack } from "@mui/system"
-import React, { useEffect } from "react"
-import { CenteredHeaderCard } from "../../../cards/CenteredHeaderCard"
-import { RouterLink } from "../../../RouterLink"
+import { Button, Chip, MenuItem, Select } from '@mui/material';
+import { Box, Stack } from '@mui/system';
+import React, { useEffect } from 'react';
+import { CenteredHeaderCard } from '../../../cards/CenteredHeaderCard';
+import { RouterLink } from '../../../RouterLink';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
-import { useState } from "react"
-import { useSelector } from "react-redux"
-import axios from "axios"
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
 import { selectAuthUserToken } from 'features/authSlice';
 
 export const AddSkillsForm = () => {
-    const [category, setCategory] = useState();
-    const [skill, setSkill] = useState('');
-    const [skills, setSkills] = useState([]);
-    const [selected, setSeletected] = useState([]);
-    const authToken = useSelector(selectAuthUserToken);
+  const [category, setCategory] = useState();
+  const [skill, setSkill] = useState('');
+  const [skills, setSkills] = useState([]);
+  const [selected, setSeletected] = useState([]);
+  const authToken = useSelector(selectAuthUserToken);
 
-    const [categories , setCategories] = useState([]);
+  const [categories, setCategories] = useState([]);
 
-    useEffect(()=>{
-        const fetchCategories = async () => {
-            try {
-                const response = await axios.get('/category/' , {
-                    headers:{
-                        Authorization: `Bearer ${authToken}`
-                    }
-                })
-                setCategories(response.data)
-            } catch (error) {
-                console.error(error)
-            }
-        }
-        fetchCategories()
-    },[authToken])
-    
-    useEffect(() => {
-      console.log(category);
-        const fetchSkills = async () => {
-            const response = await axios.get(`/category/${category}/skills`, { headers: { Authorization: `Bearer ${authToken}` }});
-            setSkills(response.data);
-        }
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await axios.get('/category/', {
+          headers: { Authorization: `Bearer ${authToken}` },
+        });
+        setCategories(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchCategories();
+  }, [authToken]);
 
-        fetchSkills();
-    }, [authToken, category]);
+  useEffect(() => {
+    const fetchSkills = async () => {
+      try {
+        const response = await axios.get(`/category/${category}/skills`, {
+          headers: { Authorization: `Bearer ${authToken}` },
+        });
+        setSkills(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchSkills();
+  }, [authToken, category]);
 
-
-    return (
+  return (
     <CenteredHeaderCard
       title={'Add your Skills'}
       footer={
@@ -57,7 +58,7 @@ export const AddSkillsForm = () => {
         </RouterLink>
       }
     >
-      <Stack spacing={2} sx={{ width: '100%' }} >
+      <Stack spacing={2} sx={{ width: '100%' }}>
         <FormControl fullWidth>
           <InputLabel id='Org-registration-country-select-label'>
             Select your Field
@@ -65,13 +66,14 @@ export const AddSkillsForm = () => {
           <Select
             labelId='Org-registration-country-select-label'
             value={category}
-            onChange={(e) => { setCategory(e.target.value) }}
+            onChange={(e) => {
+              setCategory(e.target.value);
+            }}
             label='Select your Field'
           >
-            {
-                categories.map((category) => 
-                <MenuItem value = {category.id}>{category.name}</MenuItem>)
-            }
+            {categories.map((category) => (
+              <MenuItem value={category.id}>{category.name}</MenuItem>
+            ))}
           </Select>
         </FormControl>
         <Stack direction={'row'} spacing={2}>
@@ -103,7 +105,7 @@ export const AddSkillsForm = () => {
               sx={{ height: '100%' }}
               onClick={() => {
                 setSeletected([...selected, skill]);
-                setSkills(skills.filter((item) => item !== skill))
+                setSkills(skills.filter((item) => item !== skill));
               }}
             >
               Add
@@ -117,8 +119,10 @@ export const AddSkillsForm = () => {
               color='primary'
               variant='outlined'
               onDelete={() => {
-                setSeletected(selected.filter((skillItem) => skillItem.id !== skill.id))
-                setSkills([...skills, skill])
+                setSeletected(
+                  selected.filter((skillItem) => skillItem.id !== skill.id)
+                );
+                setSkills([...skills, skill]);
               }}
               key={skill.id}
             />
