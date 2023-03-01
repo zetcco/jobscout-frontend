a824647eb9373b3f7eff6289e1e83031d22569cc
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-03-01 22:27:03 +0530

    Disconnect gracefully

--- src/components/profile/Message/Meeting.jsx
+++ src/components/profile/Message/Meeting.jsx
@@ -1,36 +1,66 @@
-import { Button, Grid, MenuItem, Select, TextField } from "@mui/material";
+import { Box, Button, FormControl, Grid, InputLabel, MenuItem, Select, TextField } from "@mui/material";
+import { Stack } from "@mui/system";
+import axios from "axios";
 import { Video } from "components/Video";
 import { selectAuthUser, selectAuthUserToken } from "features/authSlice";
 import { selectWebSocketStompClient } from "features/websocketSlice";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
 
 // const rtcPeerConnection = new RTCPeerConnection()
 let peerConnections = []
 export const Meeting = () => {
 
-    const { meetingLink } = useParams()
+    const { link } = useParams()
+    const navigate = useNavigate()
+    const [ meetingData, setMeetingData ] = useState('');
+    const authToken = useSelector(selectAuthUserToken)
+
+    useEffect(() => {
+        const fetchMeeting = async () => {
+            try {
+                const response = await axios.get(`/meeting/${link}`, { headers: { Authorization: `Bearer ${authToken}` } });
+                setMeetingData(response.data)
+            } catch (error) {
+                navigate('/home')
+            }
+        }
+
+        fetchMeeting()
+    }, [])
 
     const stompClient = useSelector(selectWebSocketStompClient)
-    const authToken = useSelector(selectAuthUserToken)
     const user = useSelector(selectAuthUser)
 
     const [ remoteVideos, setRemoteVideos ] = useState([])
 
-    const [ meetingId, setMeetingId ] = useState('');
-
-    const [ localStream, setLocalStream ] = useState('');
-    const [ devices, setDevices ] = useState([]);
+    const [ localStream, setLocalStream ] = useState({
+        audio: '',
+        video: ''
+    });
+    const [ videoDevices, setVideoDevices ] = useState([]);
+    const [ audioDevices, setAudioDevices ] = useState([]);
 
     // Set all the available devices on page load
     useEffect(() => {
         const getMediaDevices = async () => {
             const devices = await navigator.mediaDevices.enumerateDevices()
-            setDevices(devices)
+            devices.forEach(device => {
+                if (device.kind === 'videoinput')
+                    setVideoDevices((prevSate) => prevSate.some(({ deviceId }) => deviceId === device.deviceId) ? prevSate : [...prevSate, device])
+                else if (device.kind === 'audioinput')
+                    setAudioDevices((prevSate) => prevSate.some(({ deviceId }) => deviceId === device.deviceId) ? prevSate : [...prevSate, device])
+            });
         } 
         getMediaDevices()
     }, [])
+
+    const onCandidateLeave = (senderId) => {
+        console.log(remoteVideos)
+        // const rtcPeerConnection = peerConnections.find(obj => obj.peer === senderId).connection
+        // rtcPeerConnection.close()
+    }
 
     // Fires when a ICE_CANDIDATE signal has been recived by the newly joined user (this fires after OFFER and ICE_CANDIDATE signals are sent by existing user)
     const onIceCandidate = (data, senderId) => {
@@ -76,13 +106,22 @@ export const Meeting = () => {
     // Fires when 'Join' button is clicked
     const joinMeeting = async () => {
         const stream = await getLocalStream() // Get the local stream
+        console.log(stream.getAudioTracks())
         stream.getAudioTracks()[0].enabled = false // Mute the local stream audio (so it won't hear back)
-        setRemoteVideos((prevSate) => [...prevSate, stream]) // Add the stream to video element
+        setRemoteVideos((prevSate) => [...prevSate, { peer: 'local', stream }]) // Add the stream to video element
 
-        subscribe(meetingId) // Subscribe to the video chat room signaling channel
-        subscribe(meetingId, user.id) // Subsribe to the video chat room private signaling channel
+        subscribe(meetingData.link) // Subscribe to the video chat room signaling channel
+        subscribe(meetingData.link, user.id) // Subsribe to the video chat room private signaling channel
 
         sendMessage({ content: `User(id: ${user.id}) Joined` }, "JOIN") // Finally send to everyone that you have joined the chat room
+    }
+
+    const leaveMeeting = () => {
+        peerConnections.forEach(peer => { peer.connection.close() });
+        remoteVideos.forEach(video => { video.stream.getTracks().forEach(track => { track.stop() }) })
+        setRemoteVideos([])
+        unsubscribe(meetingData.link) 
+        unsubscribe(meetingData.link, user.id) 
     }
 
     // Fires when a JOIN signal have been recived by a new user
@@ -106,7 +145,7 @@ export const Meeting = () => {
     }
 
     const sendMessage = (data, type, to = null) => {
-        stompClient.send(`/room/${meetingId}${to ? "/" + to : ""}`, {}, JSON.stringify({
+        stompClient.send(`/room/${meetingData.link}${to ? "/" + to : ""}`, {}, JSON.stringify({
             senderId: user.id,
             type,
             data: JSON.stringify(data)
@@ -115,7 +154,19 @@ export const Meeting = () => {
 
     const subscribe = (meetindId, userId = null) => {
         stompClient.subscribe(
-            `/room/${meetingId}${userId ? "/" + userId : ""}`,
+            `/room/${meetingData.link}${userId ? "/" + userId : ""}`,
+            (payload) => {
+                payload = JSON.parse(payload.body)
+                if (payload.senderId !== user.id)
+                    rtcSignalResolver(payload, resolverFunctions) // Pass the recived signal to the Signal Resolver
+            },
+            {"token": authToken}
+        );
+    }
+
+    const unsubscribe = (meetindId, userId = null) => {
+        stompClient.unsubscribe(
+            `/room/${meetingData.link}${userId ? "/" + userId : ""}`,
             (payload) => {
                 payload = JSON.parse(payload.body)
                 if (payload.senderId !== user.id)
@@ -126,10 +177,12 @@ export const Meeting = () => {
     }
 
     const getLocalStream = async () => {
+        console.log(localStream)
         const constraint = {
-            video: { deviceId: { exact: localStream.deviceId } },
-            audio: true
+            video: { deviceId: { exact: localStream.video.deviceId } },
+            audio: { deviceId: { exact: localStream.audio.deviceId } }
         }
+        console.log(constraint)
         return await navigator.mediaDevices.getUserMedia(constraint)
     }
 
@@ -139,12 +192,12 @@ export const Meeting = () => {
         
         // Fires when the 'Remote description' is set for the newly created RTCPeerConnection object
         newRTCPeerConnection.ontrack = (event) => {
-            
             // Add the recived stream to the existing stream objects (which includes local stream and streams of existing users, if there's any)
             setRemoteVideos(
                 (prevSate) => {
-                    if (!prevSate.includes(event.streams[0]))
-                        return ([...prevSate, event.streams[0]])
+                    const peerStream = { peer: senderId, stream: event.streams[0] };
+                    if (!prevSate.some( ({ peer }) => peer === senderId ))
+                        return ([...prevSate, peerStream])
                     else
                         return prevSate
                 }
@@ -159,13 +212,12 @@ export const Meeting = () => {
             }
         }
 
-        newRTCPeerConnection.oniceconnectionstatechange = (event) => {
+        newRTCPeerConnection.onconnectionstatechange = (event) => {
             switch (newRTCPeerConnection.connectionState) {
                 case "disconnected":
-                    console.warn(`${senderId} disconnected`)
+                    setRemoteVideos( (prevState) => prevState.filter( (stream) => stream.peer !== senderId ) )
                     break;
                 default:
-                    console.warn(`${senderId} - Unknown`)
                     break;
             }
         }
@@ -179,36 +231,53 @@ export const Meeting = () => {
         iceCandidate: onIceCandidate, 
         offer: onOffer,
         answer: onAnswerAccept,
-        join: onCandidateJoin
+        join: onCandidateJoin,
+        leave: onCandidateLeave
     }
 
     return (
-        <>
-            <Select value={localStream} onChange={ (e) => setLocalStream(e.target.value) }>
-                {
-                    devices.map((device, index) => (
-                        <MenuItem key={index} value={device}>{device.label}</MenuItem>
-                    ))
-                } 
-            </Select>
-            <TextField value={meetingId} onChange={(e) => setMeetingId(e.target.value)}/>
-            <Button onClick={joinMeeting} variant='contained' disabled={localStream === ''}>Join</Button>
+        <Stack alignItems="center" direction="column" spacing={2}>
+            <Stack direction={{ sm: "column", md: "row" }} spacing={2}>
+                <FormControl sx={{ width: 200 }}>
+                    <InputLabel id="video-selector-label">Select Video</InputLabel>
+                    <Select label="Select Video" labelId="video-selector-label" value={localStream.video} onChange={ (e) => setLocalStream((prevState) =>  ({ ...prevState, video: e.target.value })) }>
+                        {
+                            videoDevices.map((device, index) => (
+                                <MenuItem key={index} value={device}>{device.label}</MenuItem>
+                            ))
+                        } 
+                    </Select>
+                </FormControl>
+                <FormControl sx={{ width: 200 }}>
+                    <InputLabel id="audio-selector-label">Select Audio</InputLabel>
+                    <Select label="Select Audio" labelId="audio-selector-label" value={localStream.audio} onChange={ (e) => setLocalStream((prevState) =>  ({ ...prevState, audio: e.target.value })) }>
+                        {
+                            audioDevices.map((device, index) => (
+                                <MenuItem key={index} value={device}>{device.label}</MenuItem>
+                            ))
+                        } 
+                    </Select>
+                </FormControl>
+                <Button onClick={joinMeeting} variant='contained' disabled={meetingData === ''}>Join</Button>
+                <Button onClick={leaveMeeting} variant='contained' disabled={remoteVideos.length === 0}>Disconnect</Button>
+                { user.id === meetingData.hoster?.id && (<Button onClick={joinMeeting} variant='contained' disabled={remoteVideos.length === 0}>End</Button>) }
+            </Stack>
             <Grid container>
                 {
-                    remoteVideos.map((stream, index) => (
-                        <Grid item xs={6} md={4}>
-                            <Video srcObject={stream} key={index}/>
+                    remoteVideos.map((peerStream, index) => (
+                        <Grid item xs={ remoteVideos.length === 1 ? 12 : 6} md={remoteVideos.length === 1 ? 12 : 4} key={index}>
+                            <Video srcObject={peerStream.stream}/>
                         </Grid>
                     ))
                 }
             </Grid>
-        </>
+        </Stack>
     )
 };
 
 const rtcSignalResolver = (signal, actions) => {
     let { senderId, type, data } = signal
-    const { iceCandidate, offer, answer, join } = actions
+    const { iceCandidate, offer, answer, join, leave } = actions
 
     data = JSON.parse(data)
 
@@ -221,6 +290,9 @@ const rtcSignalResolver = (signal, actions) => {
             break;
         case "ANSWER":
             answer(new RTCSessionDescription(data), senderId)
+            break;
+        case "LEAVE":
+            leave(senderId)
             break;
         case "JOIN":
             join({ senderId, data })
