9b493314b82dcb2711b459a6662560177c6b83e1
Niroshan Sanjeewa <sanjeewa11138@usci.ruh.ac.lk>
2023-03-09 16:15:26 +0530

    organizationHomeCreate

--- src/components/profile/OrganizationHome.jsx
+++ src/components/profile/OrganizationHome.jsx
@@ -6,8 +6,14 @@ import SmallPanel from '../SmallPanel'
 import AddBoxIcon from '@mui/icons-material/AddBox';
 import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
 import WorkIcon from '@mui/icons-material/Work';
+import { ScheduleMeeting } from 'components/meeting/ScheduleMeeting'
+import { VideoChatRounded } from '@mui/icons-material'
+
 
 export const OrganizationHome = () => {
+
+    const [ meetingModalOpen, setMeetingModalOpen ] = useState(false);
+    
   return (
     <Stack direction = {'column'} spacing = {4}>
      <Stack>
@@ -16,15 +22,27 @@ export const OrganizationHome = () => {
         >
                 <Grid container spacing={2}>
                     <Grid item xs={4}>
-                    <RouterLink to={"/posts/create"}>
                     <SelectableCard
                         title = { 
                                 <Stack direction = {'column'} alignItems = {'center'} justifyContent = {'center'} spacing = {1}>
-                                    <AddBoxIcon sx = {{height:'30px' , width:'30px'}}/>
-                                    <Typography variant='h6'>CREATE</Typography>
-                                </Stack> }
-                        />
-                    </RouterLink>
+                                    {/* <WorkIcon sx = {{height:'30px' , width:'30px'}}/> */}
+                                    <VideoChatRounded sx={{ height: 30, width: 30 }}/>
+                                    <Typography fontSize={17} fontWeight={650} letterSpacing={1}>MEET</Typography>
+                                </Stack> 
+                                }
+                            onClick={() => setMeetingModalOpen(true)}
+                            />
+                        <Modal
+                                open={meetingModalOpen}
+                                onClose={() => setMeetingModalOpen(false)}
+                                sx={{
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'center'
+                                }}
+                            >
+                            <ScheduleMeeting/>
+                        </Modal>
                     </Grid>
                     <Grid item xs={4}>
                     <RouterLink to={"/posts/1/manage"}>
