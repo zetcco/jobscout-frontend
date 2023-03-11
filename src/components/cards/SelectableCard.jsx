8f5e4da247ac9ce79bb3f0b4cbfecbe6fdf577ce
Thanis <77865201+thanis04@users.noreply.github.com>
2023-03-11 12:35:26 +0530

    removed unnessary library

--- src/components/cards/SelectableCard.jsx
+++ src/components/cards/SelectableCard.jsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from '@mui/material'
+import { Typography } from '@mui/material'
 import { Stack } from '@mui/system'
 import React from 'react'
 import { BasicCard } from './BasicCard'
