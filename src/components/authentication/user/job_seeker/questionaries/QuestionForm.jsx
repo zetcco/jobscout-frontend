7c3e877a7450749f5ae180271deb9542d6ff85cb
Thanis <77865201+thanis04@users.noreply.github.com>
2023-03-11 20:34:09 +0530

    each question for single component

--- src/components/authentication/user/job_seeker/questionaries/QuestionForm.jsx
+++ src/components/authentication/user/job_seeker/questionaries/QuestionForm.jsx
@@ -1,9 +1,38 @@
-import { CenteredHeaderCard } from 'components/cards/CenteredHeaderCard';
+import { BasicCard } from 'components/cards/BasicCard';
+import {
+  FormControl,
+  FormLabel,
+  RadioGroup,
+  Radio,
+  FormControlLabel,
+} from '@mui/material';
 
-export const QuestionForm  = () => {
-    return(
-        <CenteredHeaderCard>
-            
-        </CenteredHeaderCard>
-    );
-}
+export const QuestionForm = () => {
+  return (
+    <BasicCard>
+      <FormControl>
+        <FormLabel id='q1'>
+          {' '}
+          What is the difference between list and tuples in Python?
+        </FormLabel>
+        <RadioGroup aria-labelledby='q1' name='radio-buttons-group'>
+          <FormControlLabel
+            value='female'
+            control={<Radio size='small' />}
+            label='Female'
+          />
+          <FormControlLabel
+            value='male'
+            control={<Radio size='small' />}
+            label='Male'
+          />
+          <FormControlLabel
+            value='other'
+            control={<Radio size='small' />}
+            label='Other'
+          />
+        </RadioGroup>
+      </FormControl>
+    </BasicCard>
+  );
+};
