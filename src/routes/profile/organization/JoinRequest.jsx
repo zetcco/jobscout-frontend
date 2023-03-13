84864030712abc986c840c3cfcb9d4b5d7c96840
Niroshan Sanjeewa <sanjeewa11138@usci.ruh.ac.lk>
2023-03-13 14:15:20 +0530

    JoinRequestBackendJoinDisplay

--- src/routes/profile/organization/JoinRequest.jsx
+++ src/routes/profile/organization/JoinRequest.jsx
@@ -1,34 +1,49 @@
-import React from 'react'
-import { Typography , Button } from '@mui/material'
-import { Box, Stack } from '@mui/material'
-import ProfileWithHeader from 'components/profile/ProfileWithHeader'
-import { BasicCard } from 'components/cards/BasicCard'
+import { Box, Button, Stack, Typography } from '@mui/material';
+import axios from 'axios';
+import { BasicCard } from 'components/cards/BasicCard';
+import ProfileWithHeader from 'components/profile/ProfileWithHeader';
+import { selectAuthUserToken } from 'features/authSlice';
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux';
 
 export const JoinRequest = () => {
+const [requests, setRequests] = useState([]);
+const authToken =useSelector(selectAuthUserToken);
+
+const myFunction = async() =>{
+  const response = await axios.get(
+    'organization/join-request',
+    {headers:{
+      Authorization : `Bearer ${authToken}`
+    }
+    });
+    //console.log(response.data);
+    setRequests(response.data)
+}
+useEffect(() =>{
+  myFunction()
+  //console.log('Niro');
+},[])
   return (
     <BasicCard>
-        <Stack  
-            direction="row"
-            justifyContent="flex-start"
-            alignItems="center"
-            spacing={2}>
-        <Box>
-        <Stack spacing={1}>
-        <ProfileWithHeader name={'Request Name'} />
-          <Typography>
-              Details..............................................................................................................................
-          </Typography>
-        </Stack>
-         </Box> 
-         <Box>
-        <Stack 
-            direction="row"
-            spacing={2}>
-                <Button variant="outlined">ACCEPT</Button>
-                <Button variant="outlined">REJECT</Button>
+      <Stack  spacing={2} >
+      {requests.map((request,index) =>(
+        
+          <Box>
+            <Stack  justifyContent={'flex-start'} alignItems="center" direction="row" spacing={2} key={index}>
+              <ProfileWithHeader name={request.displayName} />
+              <Typography>{request.email} </Typography>
             </Stack>
-         </Box> 
+          </Box>
+          ))}
+          <Box>
+            <Stack direction={"row"} spacing={2}>
+              <Button variant='outlined'>ACCEPT</Button>
+              <Button variant='outlined'>REJECT</Button>
+            </Stack>
+          </Box>
         </Stack>
+      
     </BasicCard>
   )
 }
