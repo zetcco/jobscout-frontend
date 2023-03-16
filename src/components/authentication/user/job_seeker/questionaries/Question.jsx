710fe367c1a00f35e88be81d8c6e55ede6d242ac
Thanis <77865201+thanis04@users.noreply.github.com>
2023-03-16 06:47:06 +0530

    component linked

--- src/components/authentication/user/job_seeker/questionaries/Question.jsx
+++ src/components/authentication/user/job_seeker/questionaries/Question.jsx
@@ -1,17 +1,19 @@
 import { BasicCard } from 'components/cards/BasicCard';
-import { Stack } from '@mui/material';
+import { IconButton, Stack } from '@mui/material';
 import { RouterLink } from 'components/RouterLink';
+import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined';
+import { FormatAlignJustify } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
 
 export const Question = () => {
   return (
     <>
-      <BasicCard onClick={() => <RouterLink to={'/questionaries/python/detail'}/>}>
+      <BasicCard>
         <Stack spacing={2} direction={'row'}>
           <img
             style={{ width: 60, height: 60, margin: 'auto', padding: 0 }}
             src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT70qUCUgVzIgb_3Gt0AbED0GuWieZz-pcJLw&usqp=CAU'
             alt='python-logo'
-            
           />
           <Stack spacing={0.5} direction={'column'}>
             <h3>Python (Programming Language)</h3>
@@ -21,12 +23,22 @@ export const Question = () => {
               quo
             </p>
           </Stack>
+          <Stack justifyContent={'center'}>
+            <IconButton
+              size='large'
+              color='success'
+              component={Link}
+              to='/questionaries/python/detail'
+            >
+              <ArrowForwardIosOutlinedIcon />
+            </IconButton>
+          </Stack>
         </Stack>
       </BasicCard>
       <BasicCard>
         <Stack spacing={2} direction={'row'}>
           <img
-            style={{ width: 80, height: 80, margin: 'auto', padding: 0}}
+            style={{ width: 80, height: 80, margin: 'auto', padding: 0 }}
             src='https://www.yourrightdecision.com/blog/wp-content/uploads/2020/11/163-1.jpg'
             alt='ms-word-logo'
           />
@@ -37,6 +49,16 @@ export const Question = () => {
               commodi ducimus asperiores esse cupiditate consequuntur voluptas
               quo
             </p>
+          </Stack>
+          <Stack justifyContent={'center'}>
+            <IconButton
+              size='large'
+              color='success'
+              component={Link}
+              to='/questionaries/msword/detail'
+            >
+              <ArrowForwardIosOutlinedIcon />
+            </IconButton>
           </Stack>
         </Stack>
       </BasicCard>
