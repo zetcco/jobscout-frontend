33bb53e91403f509e68c7c8378bc0f2684b2b88c
Indrajith Madhumal <indrajithmadhumal@gmail.com>
2023-03-22 17:03:04 +0530

    Profile UI

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,38 +1,86 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BasicCard } from '../cards/BasicCard'
 import { Stack } from '@mui/system'
-import { Box, IconButton, Popover } from '@mui/material'
+import { Box, Button, IconButton, Popover } from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu';
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { selectAuthUser, selectAuthUserToken } from 'features/authSlice';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { ProfileWithFullNameSubtitle } from './ProfileWithFullNameSubtitle';
+import { ChatBubble, ChatBubbleRounded, EditRounded, StarBorder, StarRounded } from '@mui/icons-material';
 
-export const Profile = ({profile , profileActionButtons , profileRouteButtons , content}) => {
+export const Profile = ({profile , content}) => {
+
+    const { userId } = useParams()
+    const authToken = useSelector(selectAuthUserToken)
+    const authUser = useSelector(selectAuthUser)
+    const [ profileData, setProfileData ] = useState()
+
+    useEffect(() => {
+        const getProfileData = async () => {
+            const res = await axios.get(`/user/${userId}`, { headers: {
+                Authorization: `Bearer ${authToken}`
+             } })
+            console.log(res.data)
+            setProfileData(res.data)
+        }
+        getProfileData()
+    }, [userId])
 
     const [anchorEl, setAnchorEl] = useState(null);
 
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
-
-    const open = Boolean(anchorEl);
-    const id = open ? 'simple-popover' : undefined;
+    let profileRouteButtons = [];
+    let profileActionButtons = [];
+    let role = null
+    if (profileData?.id === authUser.id)
+        profileActionButtons.push(
+            <Button variant = {'outlined'} startIcon = {<EditRounded/>}>Edit Profile</Button> , 
+        )
+    else
+        profileActionButtons.push(
+            <Button variant = {'outlined'} startIcon = {<ChatBubbleRounded/>}>Message</Button> , 
+        )
+    
+    if (profileData?.role === "ROLE_JOB_SEEKER") {
+        profileRouteButtons = [ 
+            <Button variant = {'outlined'}>Recommendations</Button> , 
+            <Button variant = {'outlined'}>Posts</Button>,
+            <Button variant = {'outlined'}>Qualification</Button> , 
+            <Button variant = {'outlined'}>Portfolio</Button>
+        ]
+        role = "Job Seeker"
+        if (authUser.role === "ROLE_JOB_CREATOR")
+            profileActionButtons.push( 
+                <Button variant = {'outlined'} startIcon = {<StarRounded/>}>Recommend</Button>
+            )
+    } else if (profileData?.role === "ROLE_JOB_CREATOR") {
+        profileRouteButtons = [ 
+            <Button variant = {'outlined'}>Posts</Button>,
+        ]
+        role = "Job Creator"
+    } else if (profileData?.role === "ROLE_ORGANIZATION") {
+        profileRouteButtons = [ 
+            <Button variant = {'outlined'}>Posts</Button>, 
+            <Button variant = {'outlined'}>Gallery</Button>
+        ]
+        role = "Organization"
+    }
 
     return (
         <BasicCard>
             <Stack direction={'column'} spacing={4}>
-                <Stack direction={{ ...( profileActionButtons ? { xs: 'column', md: 'row' } : { xs: 'row' } ) }} justifyContent={'space-between'} alignItems={ !profileActionButtons ? "center" : undefined } spacing={2}>
-                    { profile }
+                <Stack direction={{ ...( profileActionButtons.length !== 0 ? { xs: 'column', md: 'row' } : { xs: 'row' } ) }} justifyContent={'space-between'} alignItems={ !profileActionButtons ? "center" : undefined } spacing={2}>
+                    <ProfileWithFullNameSubtitle name={profileData?.displayName} subtitle={role} src={profileData?.displayPicture}/>
                     {
-                        profileActionButtons && 
+                        profileActionButtons.length !== 0 && 
                         <Stack direction={"row"} alignItems="center" justifyContent={"space-between"}>
                             <Stack spacing={2} direction="row">
                                 {profileActionButtons} 
                             </Stack>
                             <Box display={{ xs: 'block', md: 'none' }}>
-                                <IconButton variant="contained" size="large" aria-describedby={id} onClick={handleClick}>
+                                <IconButton variant="contained" size="large" onClick={(e) => {setAnchorEl(e.target)}}>
                                     <MenuIcon/>
                                 </IconButton>
                             </Box>
@@ -40,9 +88,9 @@ export const Profile = ({profile , profileActionButtons , profileRouteButtons , 
                     }
 
                     {
-                        !profileActionButtons && (
+                        profileActionButtons.length === 0 && (
                             <Box display={{ xs: 'block', md: 'none' }}>
-                                <IconButton variant="contained" size="large" aria-describedby={id} onClick={handleClick}>
+                                <IconButton variant="contained" size="large" onClick={(e) => {setAnchorEl(e.target)}}>
                                     <MenuIcon/>
                                 </IconButton>
                             </Box>
@@ -50,16 +98,18 @@ export const Profile = ({profile , profileActionButtons , profileRouteButtons , 
                     }
 
                     {/* This is the Popover component of the filters on smaller screens  */}
-                    <Popover id={id} open={open} anchorEl={anchorEl} onClose={handleClose} anchorOrigin={{ vertical: 'bottom', horizontal: 'left', }}>
+                    <Popover open={Boolean(anchorEl)} anchorEl={anchorEl} onClose={() => {setAnchorEl(null)}} anchorOrigin={{ vertical: 'bottom', horizontal: 'left', }}>
                         <BasicCard>
                             <Stack direction={"column"} spacing={2}>                           
-                                {profileRouteButtons} 
+                                {profileRouteButtons ? profileRouteButtons : []} 
                             </Stack>
                         </BasicCard>
                     </Popover>
 
                 </Stack>
-                <Box display={{ xs: "none", md: "block" }}><Stack direction = {'row'} spacing={1}>{ profileRouteButtons }</Stack></Box>
+                <Box display={{ xs: "none", md: "block" }}><Stack direction = {'row'} spacing={1}>
+                    {profileRouteButtons ? profileRouteButtons : []} 
+                </Stack></Box>
                 <Stack direction = {'column'} spacing = {4}>{ content }</Stack>
             </Stack>
         </BasicCard>
--- src/components/profile/ProfileWithFullNameSubtitle.jsx
+++ src/components/profile/ProfileWithFullNameSubtitle.jsx
@@ -1,14 +1,15 @@
 import { Stack } from "@mui/system";
 import { FormHelperText ,Typography} from "@mui/material";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { AvatarWithInitials } from "components/AvatarWithInitials";
 
-export const ProfileWithFullNameSubtitle = ({title , name , subtitle}) => {
+export const ProfileWithFullNameSubtitle = ({title , name , subtitle, src}) => {
     return ( 
             <Stack direction={'column'} spacing={1}>
                     <FormHelperText>{ title }</FormHelperText>                 
-                    <Stack direction={'row'} spacing = {0.5} alignItems={'center'}>
-                        <AccountCircleIcon sx={ {fontSize:{ xs: 80, md: 100 } }}/>
-                        <Stack direction={'column'} spacing={0.5}>
+                    <Stack direction={'row'} spacing = {1.5} alignItems={'center'}>
+                        <AvatarWithInitials size={{ xs: 80, md: 100 }} src={src} name={name}/>
+                        <Stack direction={'column'}>
                             <Typography variant='profile_name'>{ name }</Typography>
                             { subtitle && <Typography variant= 'button'>{ subtitle }</Typography>  }
                         </Stack>                      
