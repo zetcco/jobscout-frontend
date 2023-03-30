7d357b7bb0557be840e1aa16c9cb24f39738af13
ANMSilva <nipunmadumal18@gmail.com>
2023-03-30 08:30:50 +0530

    CreateJobPostForm modified

--- src/components/job_postings/CreateJobPostForm.jsx
+++ src/components/job_postings/CreateJobPostForm.jsx
@@ -1,10 +1,64 @@
-import { Grid, Stack, TextField, Button } from '@mui/material';
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { Grid, Stack, TextField, Button, FormControl, InputLabel, Select, MenuItem} from '@mui/material';
 import { CenteredHeaderCard } from '../cards/CenteredHeaderCard';
+import { selectAuthUserToken } from 'features/authSlice';
+import axios from 'axios';
 
 export const CreateJobPostForm = () => {
 
+const [categories , setCategories] = useState([]);
+const [data , setData] = useState({
+  dueDate:"",
+  title: "",
+  description: "",
+  type:"",
+  urgent:"",
+  status:"",
+  category:{
+    id:"",
+    title:"",
+    description:""
+  }
+})
+
+const handle= (e) => {
+  if (e.target.name === "category")
+    setData((prev) => ({ ...prev , [e.target.name] : { id: e.target.value}}))
+  else
+    setData((prev) => ({...prev , [e.target.name] : e.target.value}))
+}
+
+const handleSubmit = (e) =>{
+   e.preventDefault();
+   axios.post('/jobpost' , data , {
+    header:{Authorization:`Bearer ${token}`}
+   })
+   .then((response)=>console.log(response))
+   .catch((err) => console.log(err))
+}
+
+const token = useSelector(selectAuthUserToken);
+
+useEffect(()=>{
+  const fetchCategories = async () => {
+      try {
+          const response = await axios.get('/category/' , {
+              headers:{Authorization: `Bearer ${token}`}
+          })
+          setCategories(response.data)
+        
+      } catch (error) {
+          console.error(error)
+      }
+  }
+  fetchCategories()
+
+  },[token])
+
   return (
     <>
+    <form onSubmit={handleSubmit}>
       <Stack>
         <CenteredHeaderCard
           title={'Create Job Post'}
@@ -13,7 +67,7 @@ export const CreateJobPostForm = () => {
               <Button variant='outlined' fullWidth>
                 Cancel
               </Button>
-              <Button variant='contained' fullWidth>
+              <Button variant='contained' fullWidth type='submit'>
                 Submit
               </Button>
             </Stack>
@@ -22,64 +76,109 @@ export const CreateJobPostForm = () => {
           <Grid container spacing={2}>
             <Grid item xs={12} md={12}>
               <TextField
-                id='outlined-basic'
-                label='Title'
-                placeholder='Enter the Title'
-                variant='outlined'
+                name = "title"
+                label = 'Title'
+                value = {data.title}
+                placeholder = 'Enter the Title'
+                variant = 'outlined'
+                onChange = {handle}
                 fullWidth
               />
             </Grid>
             <Grid item xs={12} md={12}>
               <TextField
-                id='outlined-multiline-static'
-                label='Description'
-                placeholder='Enter the Description'
+                name = "description"
+                label = 'Description'
+                value = {data.description}
+                onChange = {handle}
+                placeholder = 'Enter the Description'
                 multiline
                 minRows={3}
                 maxRows={6}
                 fullWidth
               />
             </Grid>
+            <Grid item xs={12} md={12}>
+              <FormControl fullWidth>
+                  <InputLabel id="JobPost-Creation-Category-select-label">Select Category</InputLabel>
+                  <Select
+                      labelId = "JobPost-Creation-Category-select-label"
+                      name = "category"
+                      value = {data.category.id}
+                      onChange = {handle}
+                      label = "Category"   
+                      >
+                       {
+                        categories.map((category) =>
+                        <MenuItem value = { category.id }>{ category.name }</MenuItem>)
+                       }     
+                    </Select>
+                </FormControl>
+            </Grid>
             <Grid item xs={12} md={6}>
-              <TextField
-                id='outlined-basic'
-                label='Select Category'
-                placeholder='Search for Category'
-                variant='outlined'
-                fullWidth
-              />
+            <FormControl fullWidth>
+                  <InputLabel id="JobPost-Creation-Type-select-label">Select Type</InputLabel>
+                  <Select
+                      labelId="JobPost-Creation-Type-select-label"
+                      name="type"
+                      value = {data.type}
+                      onChange = {handle}
+                      label="Type"   
+                      >  
+                        <MenuItem value = {"TYPE_PERMANENT"}>Full Time</MenuItem> 
+                        <MenuItem value = {"TYPE_PART_TIME"}>Part Time</MenuItem> 
+                        <MenuItem value = {"TYPE_FREELANCE"}>Freelance</MenuItem> 
+                    </Select>
+                </FormControl>
+            </Grid>
+            <Grid item xs={12} md={6}>
+            <FormControl fullWidth>
+                  <InputLabel id="JobPost-Creation-Status-select-label">Select Status</InputLabel>
+                  <Select
+                      labelId = "JobPost-Creation-Status-select-label"
+                      name = "status"
+                      value = {data.status}  
+                      onChange = {handle}
+                      label = "City"   
+                      >
+                        <MenuItem value = {'STATUS_ACTIVE'}>Active</MenuItem> 
+                        <MenuItem value = {'STATUS_HOLD'}>Hold</MenuItem> 
+                        <MenuItem value = {'STATUS_OVER'}>Over</MenuItem>    
+                    </Select>
+                </FormControl>
             </Grid>
             <Grid item xs={12} md={6}>
               <TextField
-                label='Date of Birth'
-                type='date'
-                placeholder='Enter your Date of Birth'
-                InputLabelProps={{ shrink: true }}
+                label = 'Due Date'
+                type = 'date'
+                name = "dueDate"
+                value = {data.dueDate}
+                onChange = {handle}
+                placeholder = 'Enter due date'
+                InputLabelProps = {{ shrink: true }}
                 fullWidth
                 required
               />
             </Grid>
             <Grid item xs={12} md={6}>
-              <TextField
-                id='outlined-basic'
-                label='Select Type'
-                placeholder='Search for Type'
-                variant='outlined'
-                fullWidth
-              />
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <TextField
-                id='outlined-basic'
-                label='Select Type'
-                placeholder='Search for Type'
-                variant='outlined'
-                fullWidth
-              />
+            <FormControl fullWidth>
+                  <InputLabel id="JobPost-Creation-Urgent-select-label">Select Urgent</InputLabel>
+                  <Select
+                      labelId = "JobPost-Creation-Urgent-select-label"
+                      name = "urgent"
+                      value = {data.urgent}
+                      onChange = {handle}
+                      label = "City"   
+                      >
+                        <MenuItem value = {true}>Urgent</MenuItem> 
+                        <MenuItem value = {false}>Not urgent</MenuItem>    
+                    </Select>
+                </FormControl>
             </Grid>
           </Grid>
         </CenteredHeaderCard>
       </Stack>
+      </form>
     </>
   );
 };
