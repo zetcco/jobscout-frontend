ad51ecd3174c37a89bf3462bb855740e93fd1624
Gimhan Mahela <gimhanmahela@gmail.com>
2023-04-11 11:22:57 +0530

    editb

--- src/components/blog/BlogPostSummary.jsx
+++ src/components/blog/BlogPostSummary.jsx
@@ -1,6 +1,6 @@
 import {Typography } from '@mui/material'
 import { Stack } from '@mui/system'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BasicCard } from '../cards/BasicCard'
 import ProfileHeaderCard from '../profile/ProfileHeaderCard'
 import MoreVertIcon from '@mui/icons-material/MoreVert';
@@ -8,27 +8,28 @@ import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
 import ModeCommentOutlinedIcon from '@mui/icons-material/ModeCommentOutlined';
 import BookmarkBorderOutlinedIcon from '@mui/icons-material/BookmarkBorderOutlined';
 import { RouterLink } from '../RouterLink'
+ 
 
-function BlogPostSummary() {
+function BlogPostSummary({name,timeStamp,content}) {
   return (
     <>
         <BasicCard>
          <Stack spacing = {2}>
             <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2}>
                 <Stack direction="row" alignItems="center" spacing={2}>
-                        <ProfileHeaderCard name={'Indrajith Madhumal'} /> 
-                        <tr>18:00 - Dec 28</tr>
+                        <ProfileHeaderCard name={name} /> 
+                        <tr>{timeStamp}</tr>
                 </Stack>
                 <MoreVertIcon />
             </Stack>
             <Stack alignItems={'flex-start'}>
                <Typography>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                    {content}
                </Typography>
             </Stack>
 
             <Stack direction="row" justifyContent="center" alignItems="center" spacing={2}>
-               <RouterLink  to={"/blog/1"}> view more</RouterLink>
+               <RouterLink  to={"/blog/1"}>view more</RouterLink>
             </Stack>
 
             <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2}>
