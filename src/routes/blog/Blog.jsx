e27465a5b30bb903dde4fccaea87550893103adf
Gimhan Mahela <gimhanmahela@gmail.com>
2023-04-11 11:22:31 +0530

    remove the right left buttons

--- src/routes/blog/Blog.jsx
+++ src/routes/blog/Blog.jsx
@@ -2,25 +2,46 @@ import { Stack } from "@mui/system"
 import {Button } from '@mui/material'
 import BlogPostSummary from "../../components/blog/BlogPostSummary"
 import SmallPanel from "../../components/SmallPanel"
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { selectAuthUserToken } from "features/authSlice";
+import axios from "axios";
 
 function Blog() {
+  const [viewBlogPost,setViewBlogPost] = useState(null);
+  const [viewBlogPosts,setViewBlogPosts] = useState([]);
+
+  const authToken = useSelector(selectAuthUserToken);
+
+  console.log(authToken);
+
+  useEffect( () => {
+    
+    loadPage()
+  }, [] );
+
+  const loadPage = async () => {
+    const response = await axios.get(
+      '/posts?page=0&size=2',
+      { headers : {
+          Authorization : `Bearer ${authToken}`
+      }}
+    );
+      setViewBlogPosts(response.data);
+  }
+
   return (
     <>
         <Stack direction="row" justifyContent="center" alignItems="flex-start" spacing={2}>
-            <SmallPanel
-            children = {[
-                <Button variant = {'outlined'} fullWidth>Left Button</Button>
-              ]}
-            />
+            
           <Stack spacing={3}>
-            <BlogPostSummary/>
-            <BlogPostSummary/>
+            {
+              viewBlogPosts.map( (blogPost, index) => (
+                <BlogPostSummary name={blogPost.user.displayName} timestamp={blogPost.timestamp} content={blogPost.content}/>
+              ) )
+            }
           </Stack>
-          <SmallPanel 
-             children = {[
-              <Button variant = {'outlined'} fullWidth>Right Button</Button>
-            ]}
-          />
+          
         </Stack>
     </>
   )
