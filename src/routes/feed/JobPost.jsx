ef15f22f5723757172dd590354a06b5855caacff
ANMSilva <nipunmadumal18@gmail.com>
2023-04-24 13:32:57 +0530

    ownership and status modified

--- src/routes/feed/JobPost.jsx
+++ src/routes/feed/JobPost.jsx
@@ -40,13 +40,28 @@ useEffect(()=>{
     return ( 
         <BasicCard>
             <Stack spacing={2} sx={{ width: '100%' }}>
-                        <Box> <SingleJobPost
-                            title = { jobPost.title }
-                            type = { jobPost.type }
-                        >{ jobPost.description }</SingleJobPost> </Box>
+                        <Box> 
+                            <SingleJobPost
+                                title = { jobPost.title }
+                                type = { jobPost.type }
+                            >{ jobPost.description }
+                            </SingleJobPost> 
+                        </Box>
                         <Stack direction={{ xs: "column", sm: "row" }} alignItems="stretch" justifyContent={"space-between"} spacing={2}>
-                            <SmallPanel mainTitle={'Ownership'} children={<Ownership/>} sx={{ flexGrow: 1 }} />
-                            <SmallPanel mainTitle={'Status'} children={<Status/>} sx={{ flexGrow: 2 }} />
+                            <SmallPanel 
+                                mainTitle={'Ownership'} 
+                                children={<Ownership 
+                                                createdBy={jobPost.jobCreator?.displayName} 
+                                                ownedBy={jobPost.organization?.displayName}
+                                                />}
+                                sx={{ flexGrow: 1 }} />
+                            <SmallPanel 
+                                mainTitle={'Status'} 
+                                children={<Status 
+                                                status = { jobPost.status }
+                                                dueDate = { jobPost.dueDate }
+                                                />} 
+                                sx={{ flexGrow: 2 }} />
                         </Stack>
                         <Button variant="contained" color='success' fullWidth size='large'>Apply now</Button>
             </Stack>                   
